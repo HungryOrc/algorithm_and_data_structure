412744e88982110691d203ff02de5dd21a69e359
HungryOrc <shu.da.the119@gmail.com>
2016-09-28 16:33:59 -0400

    Update Three Sum 草稿.java

--- Three Sum 草稿.java
+++ Three Sum 草稿.java
@@ -1,4 +1,6 @@
-要求返回的是三个数的升序组合，不要求下标所以可以利用上一题的两种方法，要求元组(a, b, c)使得a + b + c = 0，对任意一个元素A[i],只要判断数组中是否存在A[j] + A[k] = target - A[i]，这样便可以转换成 Two sum问题了。由于题目要求返回的集合无重复且元组顺序非递减，可以先对数组进行排序，而后转换成 Two sum 问题。代码如下：
+求元组(a, b, c)使得a + b + c = 0，对任意一个元素A[i],只要判断数组中是否存在A[j] + A[k] = target - A[i]，
+这样便可以转换成 Two sum问题了。
+由于题目要求返回的集合无重复且元组顺序非递减，可以先对数组进行排序，而后转换成 Two sum 问题。代码如下：
 
     public List<List<Integer>> threeSum(int[] num) {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
