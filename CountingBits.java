cda6a2bb664bd928d085a0ad3355edcf597fee9d
HungryOrc <shu.da.the119@gmail.com>
2016-10-07 10:42:04 -0400

    Update CountingBits.java

--- CountingBits.java
+++ CountingBits.java
@@ -5,15 +5,28 @@ Try to make the Runtime ~ O(n) and Space cost ~ O(n).   */
 
 public class CountingBits
 {
-
+    
+    // 方法：位运算 + 递归。看最右边的一位是否是零（i & 1），然后右移一位（i >> 1）
+    // Runtime: 应该是 O(n*logn)，但实际表现非常快
+    // Reference: https://discuss.leetcode.com/topic/40162/three-line-java-solution/2
+    //
+    public int[] countBits(int num)
+    {
+        int[] outputIntArray = new int[num + 1];
+        
+        for (int i = 1; i <= num; i++)
+            outputIntArray[i] = outputIntArray[i >> 1] + (i & 1);
+        
+        return outputIntArray;
+    }
 
 
     // 方法：除以2，再减去2^m ---- 这两步交替进行，直到本数成为0
     // Runtime：O(n*logn)，很慢，不符合题意 O(n) 的要求
     //
-    public int[] countBits(int num) {
-        
-        int[] outputIntArray = new int[num+1];
+    public int[] countBits(int num)
+    {        
+        int[] outputIntArray = new int[num + 1];
         
         for (int curNum = 0; curNum <= num; curNum++)
         {
