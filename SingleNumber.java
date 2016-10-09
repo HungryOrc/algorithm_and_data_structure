1d61c7d82b39f901e5c3036b94d8b5d8de346b43
HungryOrc <shu.da.the119@gmail.com>
2016-10-09 19:35:31 -0400

    Update SingleNumber.java

--- SingleNumber.java
+++ SingleNumber.java
@@ -6,12 +6,24 @@ public class SingleNumber {
     // Runtime: O(n), Space: no extra space
     // Reference: https://discuss.leetcode.com/topic/1916/my-o-n-solution-using-xor
     // 
-    // 关于异或的几个常用式子：A ^ A = 0, A ^ B = B ^ A, A ^ A ^ B = A ^ B ^ A = B, (A^B) ^ C = A ^ (B^C)
+    
+    /* 关于异或的几个常用式子：A ^ A = 0, A ^ B = B ^ A, A ^ A ^ B = A ^ B ^ A = B, (A^B) ^ C = A ^ (B^C)
     // 
     // 对于“结合律” (A^B) ^ C = A ^ (B^C) 的证明：
     // （简化表达：用 AB 或 A*B 表示“与”即 A&B，用 A+B 表示“或”即 A|B）
     // A ^ B = A' & B + A & B'，因为 A'&B就是A为0而B为1所带来的最后异或的1的位，A&B'就是A为1而B为0所带来的最后异或的1的位
-    // 所以：(A^B) ^ C = A'B ^ C + AB' ^ C = AB'C + A'BC' + A'BC + AB'C'
+    // 所以：(a⊕b)⊕c
+        = (a'b + ab')⊕c
+        = (a'b + ab')'c + (a'b + ab')c'
+        = (a'b)' (ab')' c + a'bc' + ab'c'
+        = (a + b')(a' + b) c + a'bc' + ab'c'
+        = abc + a'b'c + a'bc' + ab'c'
+        a⊕(b⊕c)
+        = a'(b⊕c) + a(b⊕c)'
+        = a'(b'c + bc') + a(b'c + bc')'
+        = a'b'c + a'bc' + a(b'c)'(bc')'
+        = a'b'c + a'bc' + a(b + c')(b' + c)
+        = a'b'c + a'bc' + abc + ab'c'
     //
     // 对于 A ^ A ^ B = A ^ B ^ A = B 的证明：
     // 一方面：A ^ A = 0, 0 ^ B = B
@@ -26,7 +38,7 @@ public class SingleNumber {
     // A ^ C ^ B ^ A ^ C = B = (A ^ C) ^ B ^ (A ^ C) = (A ^ C) ^ B ^ (C ^ A) = A ^ C ^ B ^ C ^ A
     // A ^ C ^ C ^ B ^ A = B
     // A ^ C ^ D ^ C ^ B ^ D ^ A = ((A^C)^D) ^ C ^ B ^ D ^ A = D ^(A^C)^C^B ^ D ^ A = D ^ (A^C^C^B) ^ D ^ A = A^C^C^B ^ A = B
-    //
+    */ 
     public int findSingleNumber_ByBitwise (int[] givenNums)
     {        
         int singleNumber = givenNums[0];
