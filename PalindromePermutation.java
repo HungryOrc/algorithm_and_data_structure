73053055280307afbf7367e4cf03acff9248eafc
HungryOrc <shu.da.the119@gmail.com>
2016-10-29 23:30:03 -0400

    Update PalindromePermutation.java

--- PalindromePermutation.java
+++ PalindromePermutation.java
@@ -2,14 +2,14 @@
 For example: "code" -> False, "aab" -> True, "carerac" -> True.
 
 Hints:
-Consider the palindromes of odd vs even length. What difference do you notice?
-Count the frequency of each character.
-If each character occurs even number of times, then it must be a palindrome. How about character which occurs odd number of times?
+1. Consider the palindromes of odd vs even length. What difference do you notice?
+2. Count the frequency of each character.
+3. If each character occurs even number of times, then it must be a palindrome. How about character which occurs odd number of times?
 */
 
 public class Solution {
 
-    // 最普通的方法，存HashMap
+    // 最普通的方法，存 HashMap
     public boolean canPermutePalindrome(String s) {
         
         char[] charArray = s.toCharArray();
