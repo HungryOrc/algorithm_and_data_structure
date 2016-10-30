f8ffa7e1dc4f4e57f3b8ce9c27113c11c4243d54
HungryOrc <shu.da.the119@gmail.com>
2016-10-29 21:34:20 -0400

    Create FlipGame.java

---
+++ FlipGame.java
@@ -0,0 +1,50 @@
+/* You are playing the following Flip Game with your friend: 
+Given a string that contains only these two characters: + and -, you and your friend take turns to flip two consecutive "++" into "--". 
+The game ends when a person can no longer make a move and therefore the other person will be the winner.
+Write a function to compute all possible states of the string after one valid move.
+For example, given s = "++++", after one move, it may become one of the following states:
+[
+  "--++",
+  "+--+",
+  "++--"
+]
+*/
+
+public class Solution {
+    
+    // 最普通的方法
+    public List<String> generatePossibleNextMoves(String s) {
+        
+        //！注意！从 String 转到 charArray ---- String.toCharArray()
+        char[] charArray = s.toCharArray();
+        List<String> output = new ArrayList<String>();
+        
+        for (int i = 0; i < charArray.length-1; i++)
+        {
+            if (charArray[i] == '+' && charArray[i+1] == '+')
+            {
+                // 这里一定要新做一个 charArray，不能用 tempCharArray = oldCharArray，那样还是同一个Reference
+                char[] tempCharArray = s.toCharArray();
+                tempCharArray[i] = '-';
+                tempCharArray[i+1] = '-';
+                
+                // ！注意！从 charArray 转到 String ---- new String(charArray)，不能少了new关键字！
+                output.add(new String(tempCharArray));
+            }
+        }
+        return output;
+    }
+    
+    
+    // 用 ArrayList.indexOf(String) 的方法：
+    // Reference: https://discuss.leetcode.com/topic/27232/4-lines-in-java
+    //
+    public List<String> generatePossibleNextMoves(String s) {
+        List list = new ArrayList();
+        for (int i=-1; (i = s.indexOf("++", i+1)) >= 0; )
+            list.add(s.substring(0, i) + "--" + s.substring(i+2));
+        return list;
+    }
+    
+    
+}
