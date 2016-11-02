8c04448f789daeb7fa7aef0f0be3664af5121cd1
HungryOrc <shu.da.the119@gmail.com>
2016-11-02 10:08:05 -0400

    Create InvertBinaryTree.java

---
+++ InvertBinaryTree.java
@@ -0,0 +1,36 @@
+/* Invert a binary tree:
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+to:
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+
+Definition for a binary tree node.
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}   */
+
+// Reference: https://discuss.leetcode.com/topic/16039/straightforward-dfs-recursive-iterative-bfs-solutions/2
+
+public class Solution {
+    // Recursive DFS
+    /* Time: O(n), Space: O(n)。理论上Recursive的方法应该较慢，但实测这个并不比后面的Iterative的方法慢
+     This solution is bound to the application stack, which means it's no so much scalable, 
+     you can find the problem size that will OVERFLOW the STACK and crash your application. 
+     So more robust solution would be to use Stack data structure, as we show later.
+    */
+
+
+}
+
+
+
