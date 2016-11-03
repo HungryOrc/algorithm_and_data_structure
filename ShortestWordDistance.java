6b0022aed56ede390fb0d04d5252a9ecbd008ec4
HungryOrc <shu.da.the119@gmail.com>
2016-11-02 22:47:55 -0400

    Update ShortestWordDistance.java

--- ShortestWordDistance.java
+++ ShortestWordDistance.java
@@ -8,7 +8,7 @@ Note: You may assume that word1 does not equal to word2, and word1 and word2 are
 public class Solution {
 
     // 把2个字分别挑出来形成两个ArrayList，带上各自的位置作为int值存在两个ArrayList里，再进行两两比较
-    // Time: O(n^2), Space: O(1)
+    // Time: O(n^2). Space: O(1), since no additional space is used
     public int shortestDistance(String[] words, String word1, String word2) {
         
         ArrayList<Integer> occuranceWord1 = new ArrayList<>();
@@ -33,4 +33,40 @@ public class Solution {
         }
         return minDist;
     }
+    
+    
+    // One Pass
+    // Ref: https://leetcode.com/articles/shortest-word-distance/
+    /* Keep 2 indices i1 and i2 where we store the most recent locations of word1 and word2. 
+     Each time we find a new occurrence of one of the words, we do not need to search the entire array for the other word, 
+     since we already have the index of its most recent occurrence.
+     Time: O(n). Space: O(1).
+    */
+    public int shortestDistance_OnePass(String[] words, String word1, String word2) {
+        
+        int i1 = -1, i2 = -1;
+        int minDist = words.length - 1;
+        boolean indexChanged = false;
+        for (int i = 0; i < words.length; i ++)
+        {
+            if (words[i].equals(word1))
+            {
+                i1 = i;
+                indexChanged = true;
+            }
+            else if (words[i].equals(word2))
+            {
+                i2 = i;
+                indexChanged = true;
+            }
+            
+            if (indexChanged && i1 != -1 && i2 != -1)
+            {
+                minDist = Math.min(minDist, Math.abs(i1-i2));
+                indexChanged = false;
+            }
+        }
+        return minDist;
+    }
+    
 }
