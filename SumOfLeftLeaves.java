f51958707fb9eeb272d568360c8d8cf51c03a986
HungryOrc <shu.da.the119@gmail.com>
2016-11-03 22:23:27 -0400

    Update SumOfLeftLeaves.java

--- SumOfLeftLeaves.java
+++ SumOfLeftLeaves.java
@@ -32,4 +32,64 @@ public class Solution {
         
         return sumOfLeft;
     }
+    
+    
+    // Iterative DFS。自己想出来的，纪念一下
+    public int sumOfLeftLeaves(TreeNode root) {
+        
+        if (root == null)
+            return 0;
+            
+        int sumOfLeft = 0;
+        Stack<TreeNode> nodeStack = new Stack<>();
+        nodeStack.push(root);
+        
+        while(!nodeStack.isEmpty())
+        {
+            TreeNode curNode = nodeStack.pop();
+            
+            if (curNode.left != null && curNode.left.left == null && curNode.left.right == null)
+                sumOfLeft += curNode.left.val;
+            else if (curNode.left != null)
+                nodeStack.push(curNode.left);
+                
+            if (curNode.right != null)
+                nodeStack.push(curNode.right);
+        }
+        return sumOfLeft;
+    }
+    
+    
+    // Iterative BFS。自己想出来的，纪念一下
+    public int sumOfLeftLeaves(TreeNode root) {
+        
+        if (root == null)
+            return 0;
+            
+        int sumOfLeft = 0;
+        Queue<TreeNode> nodeQueue = new LinkedList<>();
+        nodeQueue.offer(root);
+        
+        while(!nodeQueue.isEmpty())
+        {
+            int numOfRemainingNodesInThisLevel = nodeQueue.size();
+            
+            while (numOfRemainingNodesInThisLevel > 0)
+            {
+                TreeNode curNode = nodeQueue.poll();
+                
+                if (curNode.left != null && curNode.left.left == null && curNode.left.right == null)
+                    sumOfLeft += curNode.left.val;
+                else if (curNode.left != null)
+                    nodeQueue.offer(curNode.left);
+                    
+                if (curNode.right != null)
+                    nodeQueue.offer(curNode.right);
+                    
+                numOfRemainingNodesInThisLevel --;
+            }
+        }
+        return sumOfLeft;
+    }
+    
 }
