91b4750cc9f489fdf3055011ae9212552d480dd2
HungryOrc <shu.da.the119@gmail.com>
2016-11-05 21:27:29 -0400

    Update MajorityElement.java

--- MajorityElement.java
+++ MajorityElement.java
@@ -30,12 +30,12 @@ public class Solution {
     public int majorityElement(int[] nums) {
 
         int major = nums[0], count = 1;
-        for(int num : nums)
+        for(int i = 1; i < nums.length; i++)
         {
             if(count == 0){
                 count = 1;
-                major = num;
-            }else if(major == num)
+                major = nums[i];
+            }else if(major == nums[i])
                 count++;
             else count--;
         }
