a005dcf5ead3c26aaac117c38bf13c00c4a525ca
HungryOrc <shu.da.the119@gmail.com>
2016-11-06 13:38:16 -0500

    Create BinaryWatch.java

---
+++ BinaryWatch.java
@@ -0,0 +1,32 @@
+/* A binary watch has 4 LEDs on the top which represent the hours (0-11), and the 6 LEDs on the bottom represent the minutes (0-59).
+Each LED represents a zero or one, with the least significant bit on the right.
+Given a non-negative integer n which represents the number of LEDs that are currently on, 
+return all possible times the watch could represent.
+Example: Input: n = 1, Return: ["1:00", "2:00", "4:00", "8:00", "0:01", "0:02", "0:04", "0:08", "0:16", "0:32"]
+Note:
+* The order of output does not matter.
+* The hour must not contain a leading zero, for example "01:00" is not valid, it should be "1:00".
+* The minute must be consist of two digits and may contain a leading zero, for example "10:2" is not valid, it should be "10:02". */
+
+public class Solution
+{
+   // Just go through the possible times and collect those with the correct number of one-bits
+   // Ref: https://discuss.leetcode.com/topic/59374/simple-python-java/1
+   public List<String> readBinaryWatch (int num)
+   {
+      List<String> times = new ArrayList<>();
+      for (int h = 0; h <= 11; h++)
+      {
+         for (int m = 0; m <= 59; m++)
+         
+            // 注意！static method of Integer Class: Integer.bitCount(int i)
+            // Returns the number of one-bits in the two's complement binary representation of the specified int value
+            if (Integer.bitCount(h * 64 + m) == num)
+               // 注意！ %02d 的意思是：整数，最多2位，十位不存在时用0补足
+               times.add (String.format("%d:%02d", h, m));
+         }
+         return times;
+      }
+   }
+       
+}
