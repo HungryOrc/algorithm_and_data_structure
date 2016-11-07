abfd8e588dd5701795cd8fc88b32ffd057c219e4
HungryOrc <shu.da.the119@gmail.com>
2016-11-07 10:51:07 -0500

    Update RomanToInteger.java

--- RomanToInteger.java
+++ RomanToInteger.java
@@ -2,15 +2,15 @@
 
 Roman Numerals Chart:
 
-I	    1   |	X	    10  |	C	    100  |	M	         1000
-II	  2   |	XX	  20  |	CC	  200  |	MM	       2000
-III	  3   |	XXX	  30  |	CCC	  300  |	M M M	     3000
-IV	  4   |	XL	  40  |	CD	  400  |	M M M M	   4000
-V	    5   |	L     50  |	D	    500  |	M M M M M  5000
-VI	  6   |	LX	  60  |	DC	  600  |	etc...
-VII	  7   |	LXX	  70  |	DCC	  700  |
-VIII	8   |	LXXX	80  |	DCCC	800  |
-IX	  9   |	XC	  90  |	CM	  900  |
+I	    1   |	X	    10  |	C	    100  |	M	        1000
+II     2   |	XX     20  |	CC     200  |	MM	        2000
+III    3   |	XXX    30  |	CCC    300  |	M M M	     3000
+IV     4   |	XL     40  |	CD     400  |	M M M M    4000
+V	    5   |	L      50  |	D      500  |	M M M M M  5000
+VI     6   |	LX     60  |	DC     600  |	etc...
+VII	 7   |	LXX    70  |	DCC    700  |
+VIII   8   |	LXXX   80  |	DCCC   800  |
+IX     9   |	XC     90  |	CM     900  |
 */
 
 public class Solution
