689b1318a424162dc5efe2a2a50dd9ee1426d93c
HungryOrc <shu.da.the119@gmail.com>
2016-11-07 09:22:41 -0500

    Update IntersectionOfTwoArraysII.java

--- IntersectionOfTwoArraysII.java
+++ IntersectionOfTwoArraysII.java
@@ -10,9 +10,11 @@ Follow up questions:
 2. What if nums1's size is small compared to nums2's size? Which algorithm is better
    我认为是用较短的array来构建HashMap，用较长的那个来打擂HashMap
 3. What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
-   // Ref:
-   
-   
+   // Ref: https://discuss.leetcode.com/topic/45992/solution-to-3rd-follow-up-question
+   * If only nums2 cannot fit in memory, put all elements of nums1 into a HashMap, 
+     read chunks of array nums2 that fit into the memory, and record the intersections.
+   * If both nums1 and nums2 are so huge that neither fit into the memory, SORT them individually (external sort), 
+     then read 2 elements from each array at a time in memory, record intersections.  */   
 
 public class Solution {
     
