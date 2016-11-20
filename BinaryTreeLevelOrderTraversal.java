6135c1d47581d988876827fcee75648a28e3ae36
HungryOrc <shu.da.the119@gmail.com>
2016-11-19 22:24:00 -0500

    Update BinaryTreeLevelOrderTraversal.java

--- BinaryTreeLevelOrderTraversal.java
+++ BinaryTreeLevelOrderTraversal.java
@@ -53,4 +53,39 @@ public class Solution {
         }
         return result;
     }
+    
+    // DFS Recursion，很巧妙！
+    // Ref: https://discuss.leetcode.com/topic/47255/java-1ms-dfs-recursive-solution-and-2ms-bfs-iterative-solution
+    // 不是“先从上到下，再从左到右的”一行一行填，而是“先从左到右，再从上到下”的一列一列填！！！
+    // 每一行即每一level的ArrayList都是从最左边开始先开个头，然后逐步向右填满
+    // 在过程中每个ArrayList都是不完整的！！而最后大家都变得完整
+    // 很巧妙！
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        
+        ArrayList<List<Integer>> result = new ArrayList<List<Integer>>();
+        if (root == null)
+            return result;
+        
+        recursionDFS(root, 0, result);
+        return result;
+    }
+    private void recursionDFS(TreeNode curRoot, int curLevel, ArrayList<List<Integer>> result)
+    {
+        if (curRoot == null)
+            return;
+        
+        // 最精妙在下面的这一句了！！！
+        if (curLevel >= result.size())
+        {
+            ArrayList<Integer> valuesInCurLevel = new ArrayList<>();
+            valuesInCurLevel.add(curRoot.val);
+            result.add(valuesInCurLevel); // 此时valuesInCurLevel这个ArrayList还不完整，但不要紧，后面会补齐
+        }
+        else
+            result.get(curLevel).add(curRoot.val); // ！！
+        
+        recursionDFS(curRoot.left, curLevel+1, result);
+        recursionDFS(curRoot.right, curLevel+1, result);
+    }
+    
 }
