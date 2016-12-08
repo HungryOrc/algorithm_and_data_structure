198505535a7b5af91ffcdbbe815eb730fc32e565
HungryOrc <shu.da.the119@gmail.com>
2016-12-08 13:44:57 -0500

    Update NthDigit.java

--- NthDigit.java
+++ NthDigit.java
@@ -12,49 +12,51 @@ Explanation: The 11th digit of the sequence 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, .
 
 public class Solution {
     
-    // 最直觉化的方法。经过的test case都通过了，但时间效率低，所以不足以遍历所有test case    
+    // 最直觉化的方法    
     // 尽量别用 Math.pow(n,m)！！会非常慢的！！
     public int findNthDigit(int n) {
         
         // 当前处于几位数。1意味着1-9(本题中不含0)，2意味着10-99，3意味着100-999……
         int curDigit = 1;
+        
+        // 用几个 long！否则 int 会不够用！
+        
         // 当前的位数里，一共包含多少个数，比如1位数包含9个，2位数包含90个，3位数包含900个……
-        int numOfNumbersInCurDigit = 9;
+        long numOfNumbersInCurDigit = 9;
         // 当前的位数里，一共包含多少位，比如1位数包含9位，2位数包含180位，3位数包含2700位……
-        int totalDigitsOfNumbersInCurDigit = 0;
+        long totalDigitsOfNumbersInCurDigit = 0;
         // 给的数n，经过上述的逐位数的盘剥后，还剩多少
         int remainingN = n;
         // 累计经过了多大的数
-        int cumulativeNum = 0;
-        
-        // 如果remainingN > numOfNumbersInCurDigit * curDigit，
-        // 意味着 n 在完全铺满本位数之后，还“有得剩”，即我们要找的数是大于当前位数的数
+        long cumulativeNum = 0;
+
         while (remainingN > numOfNumbersInCurDigit * curDigit)
         {
             totalDigitsOfNumbersInCurDigit = numOfNumbersInCurDigit * curDigit;
             remainingN -= totalDigitsOfNumbersInCurDigit;
             
-            numOfNumbersInCurDigit *= 10; // 9, 90, 900, 9000...
             cumulativeNum += numOfNumbersInCurDigit; // 9, 99, 999, 9999...
+            numOfNumbersInCurDigit *= 10; // 9, 90, 900, 9000...
             curDigit++;
         }
         
-        // 这表示我们最终停止在哪个数上（下面的其实是最终数的前一个数）。这个数由两部分加和而成：
-        // 第一部分表示n能超越的最大的整个位数，比如9、99、999之类的
-        // 第二部分表示n在当前的最后的位数里，又前进了几个数。比如在99的基础上再加3个数就是(100, 101,) 102
-        int curNumber = cumulativeNum + Math.ceil(remainingN / curDigit); 
-        char[] curNumberToCharArray = String.valueOf(curNumber).toCharArray();
-        
-        // 这表示我们经过了最后的一个整的数以后，还需要最后前进几位
+        // 这表示我们最终停止在哪个数上。这个数是前期的累计数往后再挪几个
+        // 比如在99的基础上再挪3个数就是(100, 101,) 102
+        cumulativeNum += remainingN / curDigit;
         int remainingDigitsInCurNumber = remainingN % curDigit;
+        // 如果n里余下的位数不能被 curDigit 整除的话，那么还得再往前挪一个数
+        if (remainingDigitsInCurNumber > 0)
+             cumulativeNum++; 
+        
+        char[] curNumberToCharArray = String.valueOf(cumulativeNum).toCharArray();
+        
         int result = 0;
-        // 如果最后需要前进 0 位，那么我们要的就是当前数的最后一位
         if (remainingDigitsInCurNumber == 0)
+            // 如果整除了，那么我们要的就是当前数的最后一位
             result = curNumberToCharArray[curNumberToCharArray.length - 1];
         else
             result = curNumberToCharArray[remainingDigitsInCurNumber - 1];
         
-        // char 转为 int 的方式！
         return Character.getNumericValue(result); 
     }
     
