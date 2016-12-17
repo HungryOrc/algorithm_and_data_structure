190b1b2cac85603cb19af1654a79a078827bce67
HungryOrc <shu.da.the119@gmail.com>
2016-12-17 10:29:02 -0500

    Update TwoSumIII-DataStructureDesign.java

--- TwoSumIII-DataStructureDesign.java
+++ TwoSumIII-DataStructureDesign.java
@@ -5,26 +5,52 @@ find - Find if there exists any pair of numbers which sum is equal to the value.
 For example,
 add(1); add(3); add(5);
 find(4) -> true
-find(7) -> false */
+find(7) -> false    */
 
 // Your TwoSum object will be instantiated and called as such:
 // TwoSum twoSum = new TwoSum();
 // twoSum.add(number);
 // twoSum.find(value);
 
-
-
+// Ref: https://leetcode.com/problems/two-sum-iii-data-structure-design/
+/* I use HashMap to store times of number be added.
+When find be called, we iterate the keys of HashMap, then find another number minus by value.
+Then combine the detections together. */
+public class TwoSum 
+{
+    private HashMap<Integer, Integer> myMap = new HashMap<Integer, Integer>();
+    
+    public void add(int number)
+    {
+        myMap.put(number, myMap.containsKey(number) ? myMap.get(number)+1 : 1);
+    }
+    public boolean find(int value)
+    {
+        // 注意！！！Map.Entry类型！！！
+        // 注意！！！myMap.entrySet() 方法！！！
+        for (Map.Entry<Integer, Integer> entry : myMap.entrySet())
+        {
+            int num1 = entry.getKey();
+            int num2 = value - num1;
+            if ((num1==num2 && entry.getValue()>1) || (num1!=num2 && myMap.containsKey(num2)))
+                return true;
+        }
+        return false;
+    }
+}
 
 
 // 我自己的方法。用ArrayList存数。速度慢
 public class TwoSum 
 {
     ArrayList<Integer> nums;
+    
     // Constructor
     public TwoSum()
     {
         nums = new ArrayList<Integer>();    
     }
+    
     // Add the number to an internal data structure.
     public void add(int number) {
 
