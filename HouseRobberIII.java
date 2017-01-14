940fcb8c7538521ed6137cd3129b20d8ba3b338e
HungryOrc <shu.da.the119@gmail.com>
2017-01-13 21:31:03 -0500

    Update HouseRobberIII.java

--- HouseRobberIII.java
+++ HouseRobberIII.java
@@ -30,16 +30,15 @@ Maximum amount of money the thief can rob = 4 + 5 = 9.
 
 // 这一篇的几个答案都很不错！！
 // Ref: https://discuss.leetcode.com/topic/39834/step-by-step-tackling-of-the-problem
+// 里面有很长很详细的思维过程，逐步优化算法的来龙去脉，没有抄过来，可以去这个链接看
 
 public class Solution
 {
-    // 认真思考就能发现，其实最终答案要么是第0（即root）+ 2 + 4 + 6 + 8... 层之和
-    // 要么是第 1 + 3 + 5 + 7... 层之和。这二者中的一个
      // 以下这个 recursion 方法，直观，代码简明，但速度很慢
-    public int rob(TreeNode root) 
-    {
+     public int rob(TreeNode root) 
+     {
         if (root == null) return 0;
-        
+
         int curNode_AndGrandChildren_AndOneLevelInEveryTwoLevels = root.val;
         if (root.left != null)
             curNode_AndGrandChildren_AndOneLevelInEveryTwoLevels += 
@@ -47,10 +46,57 @@ public class Solution
         if (root.right != null)
             curNode_AndGrandChildren_AndOneLevelInEveryTwoLevels += 
                 rob(root.right.left) + rob(root.right.right);
-        
-        // 其中 rob(root.left) + rob(root.right) 即当前node的两个children 及这两个children
-        // 的grandChildren，的grandChildren... 每过一个层级取一个层级，然后这一系列层级的和
+
         return Math.max(curNode_AndGrandChildren_AndOneLevelInEveryTwoLevels, 
                         rob(root.left) + rob(root.right));
-    }
+     }
+     
+     
+     // 改进上面算法后的算法
+     // 用一个HashMap把每个node及以它为root的subtree的max value都存下来
+     // 这样就减免了巨量的重复运算，大幅提高了速度
+     public int rob(TreeNode root) 
+     {
+        HashMap<TreeNode,Integer> nodeMap = new HashMap<>();
+        return robSub(root, nodeMap);
+     }
+     private int robSub(TreeNode root, Map<TreeNode,Integer> map) 
+     {
+        if (root == null) 
+            return 0;
+
+        if (map.containsKey(root)) 
+            return map.get(root);
+
+        int val = 0;
+        if (root.left != null)
+            val += robSub(root.left.left, map) + robSub(root.left.right, map);
+        if (root.right != null)
+            val += robSub(root.right.left, map) + robSub(root.right.right, map);
+
+        val = Math.max(val + root.val, robSub(root.left, map) + robSub(root.right, map));
+        map.put(root, val);
+
+        return val;
+     }
+     
+     
+     // 进一步改进为一个 Greedy Algorithm
+     // 没有细看，回头看上面链接里对于这个方法的详细解释吧
+     public int rob(TreeNode root) {
+         int[] res = robSub(root);
+         return Math.max(res[0], res[1]);
+     }
+     private int[] robSub(TreeNode root) {
+         if (root == null) return new int[2];
+
+         int[] left = robSub(root.left);
+         int[] right = robSub(root.right);
+         int[] res = new int[2];
+
+         res[0] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+         res[1] = root.val + left[0] + right[0];
+
+         return res;
+     }
 }
