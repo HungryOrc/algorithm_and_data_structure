45f39b93fdd4ecd14c649c601ca0084c10d13119
HungryOrc <shu.da.the119@gmail.com>
2017-01-18 15:25:04 -0500

    Update Permutations.java

--- Permutations.java
+++ Permutations.java
@@ -63,9 +63,47 @@ class Solution {
   
   
     // Non-Recursion 方法
-  
-  
-  
-  
+    // Ref: https://discuss.leetcode.com/topic/6377/my-ac-simple-iterative-java-python-solution
+    /* the basic idea is, to permute n numbers, we can add the nth number into the resulting List<List<Integer>> from the n-1 numbers, 
+     in every possible position.
+     For example, if the input num[] is {1,2,3}: First, add 1 into the initial List<List<Integer>> (let's call it "answer").
+     Then, 2 can be added in front or after 1. So we have to copy the List<Integer> in answer (it's just {1}), 
+     add 2 in position 0 of {1}, then copy the original {1} again, and add 2 in position 1. 
+     Now we have an answer of {{2,1},{1,2}}. There are 2 lists in the current answer.
+     Then we have to add 3. first copy {2,1} and {1,2}, add 3 in position 0; then copy {2,1} and {1,2}, and add 3 into position 1, 
+     then do the same thing for position 3. Finally we have 2*3=6 lists in answer, which is what we want. */
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> results = new ArrayList<List<Integer>>();
+        
+        if (nums == null) {
+            return results;
+        }
+        else if (nums.length == 0) {
+            results.add(new ArrayList<Integer>());
+            return results;
+        }
+        
+        int len = nums.length;
+        ArrayList<Integer> firstList = new ArrayList<>();
+        firstList.add(nums[0]);
+        results.add(firstList);
+        
+        for (int i = 1; i < len; i++) {
+            int curValue = nums[i];
+            List<List<Integer>> tmpResults = new ArrayList<List<Integer>>();
+            
+            for (List<Integer> curList : results) {
+                int curListLen = curList.size();
+                
+                for (int j = 0; j <= curListLen; j++) {
+                    List<Integer> tmpList = new ArrayList<>(curList);
+                    tmpList.add(j, curValue);
+                    tmpResults.add(tmpList);
+                }
+            }
+            results = tmpResults;
+        }
+        return results;
+    }
   
 }
