c5c45434770a3b8a2b28ec5ce74df662017a7ded
HungryOrc <shu.da.the119@gmail.com>
2017-01-17 20:21:31 -0500

    Update strStr.RabinKarp.java

--- strStr.RabinKarp.java
+++ strStr.RabinKarp.java
@@ -1,4 +1,5 @@
-// 这个算法的时间复杂度是 O(m + n)，与 KMP 相同，但理解起来比 KMP 简单
+// Rabin-Karp 算法的时间复杂度是 O(m + n)，与 KMP 相同，但理解起来比 KMP 简单
 // 这个算法还能加深对 HashMap 的理解
 // Ref:
+// https://en.wikipedia.org/wiki/Rabin%E2%80%93Karp_algorithm
 // http://www.jiuzhang.com/video/rabin-karp
