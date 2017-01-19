d21adc8ad34d4c88e8823a5644f162d1e717eb9e
HungryOrc <shu.da.the119@gmail.com>
2017-01-19 13:49:40 -0500

    Update and rename !Summary: BinarySearch - FindPosition.java to !Summary_BinarySearch_FindPosition.java

---
+++ !Summary_BinarySearch_FindPosition.java
@@ -0,0 +1,13 @@
+/**
+ * @param nums: an integer array sorted in ascending order
+ * @param target: an integer
+ * @return: an integer
+ */
+
+// Template for Binary Search - Find Position
+// 九章版本
+public int findPosition(int[] nums, int target) {
+  
+    if (nums == null || nums.length == 0) {
+
+}
