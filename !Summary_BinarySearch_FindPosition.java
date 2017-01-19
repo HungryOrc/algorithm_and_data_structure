11d6f9d68bfe8a810e2b4fbd2573a4e6dd13c07d
HungryOrc <shu.da.the119@gmail.com>
2017-01-19 13:55:07 -0500

    Update !Summary_BinarySearch_FindPosition.java

--- !Summary_BinarySearch_FindPosition.java
+++ !Summary_BinarySearch_FindPosition.java
@@ -6,8 +6,32 @@
 
 // Template for Binary Search - Find Position
 // 九章版本
+// Ref: http://www.jiuzhang.com/solutions/binary-search/
 public int findPosition(int[] nums, int target) {
   
     if (nums == null || nums.length == 0) {
-
+        return -1;
+    }  
+  
+    int start = 0, end = nums.length - 1;
+    // 注意！！while 条件的选择
+    while (start + 1 < end) {
+        // 不用 (start + end) / 2，是为了避免数据超出int的范围
+        int mid = start + (end - start) / 2;
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            start = mid; // 注意！！
+        } else {
+            end = mid; // 注意！！
+        }
+    }
+    
+    if (nums[start] == target) {
+        return start;
+    } else if (nums[end] == target) {
+        return end;
+    }
+    return -1;
 }
+
