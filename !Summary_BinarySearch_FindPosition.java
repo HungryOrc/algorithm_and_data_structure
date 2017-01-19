244cf10a6aa43fc2d9fbb7106c0b25278f1cb0b6
HungryOrc <shu.da.the119@gmail.com>
2017-01-19 14:10:24 -0500

    Update !Summary_BinarySearch_FindPosition.java

--- !Summary_BinarySearch_FindPosition.java
+++ !Summary_BinarySearch_FindPosition.java
@@ -35,3 +35,29 @@ public int findPosition(int[] nums, int target) {
     return -1;
 }
 
+
+// Template for Binary Search - Find Position
+// 普世价值版本
+public int findPosition(int[] nums, int target) {
+
+    if (nums == null || nums.length == 0) {
+        return -1;
+    }
+    
+    int start = 0, end = nums.length - 1;
+    while (start < end) {
+        int mid = start + (end - start) / 2;
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
+    }
+    
+    if (nums[start] == target) { // 此时应有 start == end 了
+        return start;
+    }
+    return -1;
+}
