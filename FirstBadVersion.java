f69f61f8d269fe7cf5ebd90c886379b0f83b1503
HungryOrc <shu.da.the119@gmail.com>
2017-01-22 23:50:36 -0500

    Update FirstBadVersion.java

--- FirstBadVersion.java
+++ FirstBadVersion.java
@@ -35,4 +35,26 @@ public class Solution extends VersionControl
         return start;
     }
 
+
+    // 九章式二分
+    public int findFirstBadVersion(int n) {
+        
+        int start = 1, end = n;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;            
+            if (isBadVersion(mid)) {
+                end = mid;
+            }
+            else {
+                start = mid;
+            }
+        }
+        
+        if (isBadVersion(start)) {
+            return start;
+        } else {
+            return end;
+        }
+    }
+    
 }
