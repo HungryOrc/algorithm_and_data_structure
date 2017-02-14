8476db032e6bfbb8e4ebee79debeaf4afb17e938
HungryOrc <shu.da.the119@gmail.com>
2017-02-14 11:57:17 -0500

    Update IntersectionOfTwoLinkedLists.java

--- IntersectionOfTwoLinkedLists.java
+++ IntersectionOfTwoLinkedLists.java
@@ -26,21 +26,19 @@ Your code should preferably run in O(n) time and use only O(1) memory.
  
 public class Solution {
     
-    // 用HashSet做，时间 O(n)，空间 O(n)。空间方面不满足 O(1) 的要求
+    // 方法1: 用HashSet做，时间 O(n)，空间 O(n)。空间方面不满足 O(1) 的要求
     // 注意！！！HashSet 是可以存 Reference 变量并比较 Reference 变量的！！！不是只能存 值变量！！！
     public ListNode getIntersectionNode(ListNode headA, ListNode headB)
     {
         HashSet<ListNode> nodeSetA = new HashSet<>();
         
-        // 将一个List填注到HashSet里去
-        while (headA != null)
-        {
+        // 将 ListA 填注到HashSet里去
+        while (headA != null) {
             nodeSetA.add(headA);
             headA = headA.next;
         }
         // 然后逐个比对另一个List里的nodes，看是否在HashSet里
-        while (headB != null)
-        {
+        while (headB != null) {
             if (nodeSetA.contains(headB))
                 return headB;
             headB = headB.next;
@@ -49,7 +47,7 @@ public class Solution {
     }
     
     
-    // 用两个指针。很巧妙的方法！！！我没想到
+    // 方法2: 两个指针，到尾之后再折回来！很巧妙！
     // Ref: https://leetcode.com/articles/intersection-two-linked-lists/
     public ListNode getIntersectionNode(ListNode headA, ListNode headB)
     {
@@ -61,39 +59,39 @@ public class Solution {
         boolean headAShifted = false;
         boolean headBShifted = false;
         
-        while (headA != headB)
-        {
+        while (headA != headB) {
             headA = headA.next;
             headB = headB.next;
             
-            // 两个pointer可能同时到达尾端，也可能不同时。但这不要紧
-            if (headA == null)
-            {
-                if (headAShifted == false)
-                {
+            // 两个pointer可能同时到达尾端，也可能不同时。但这不要紧：
+            // 情况一：如果 pointer A 到了 listA 的尾端
+            if (headA == null) {
+                // 如果pointer A 还没有被转到listB的头部过，就把它转到listB的头部
+                if (headAShifted == false) {
                     headA = originalHeadB;
                     headAShifted = true;
                 }
-                // if headA had been shifted once, then headA become null again means
+                // if headA had been shifted once, then it become null again means
                 // there is no intersection between the 2 lists
                 else
                     return null;
             }
-            if (headB == null)
-            {
-                if (headBShifted == false)
-                {
+            // 情况二：如果 Pointer B 到了 listB 的尾端
+            if (headB == null) {
+                if (headBShifted == false) {
                     headB = originalHeadA;
                     headBShifted = true;
                 }
-                // if headB had been shifted once, then headA become null again means 
+                // if headB had been shifted once, then it become null again means 
                 // there is no intersection between the 2 lists
                 else
                     return null;
             }
         }
-        // 结束了这个while loop，又没有触发 return null的话，意味着找到了交点
-        return headA;
+      
+        // 结束了这个while loop，没有触发 return null，此时有 headA == headB，
+        // 其实就是 Pointer A == Pointer B，意味着找到了交点
+        return headA; // return headB 也一样
     }
     
     
