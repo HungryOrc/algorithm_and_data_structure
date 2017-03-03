e42193c19dd06b9a42896473758d82bf12ebb44c
HungryOrc <shu.da.the119@gmail.com>
2017-03-03 15:19:54 -0500

    Update PermutationsOfNumbers_AllNumbersUnique.java

--- PermutationsOfNumbers_AllNumbersUnique.java
+++ PermutationsOfNumbers_AllNumbersUnique.java
@@ -5,7 +5,6 @@
 
 /* Given an Array of numbers, return all possible permutations.
 Notice: You can assume that there is no duplicate numbers in the list.
-
 Example:
 For nums = [1,2,3], the permutations are:
 [
@@ -51,11 +50,11 @@ class Solution {
         // 整个算法的精华在这里！每一次都是试图把整个数组里的每一个数都轮流放进去！！！
         // 只是一开始要用 ArrayList.contains 函数来判断是否有过这个数了！！！
         for (int n : nums) {
-            // ArrayList 也有 contains 函数！不光是 HashSet 有！
-            if (!curList.contains(n)) {
+            
+            if (!curList.contains(n)) { // ArrayList 也有 contains 函数！不光是 HashSet 有！
                 curList.add(n);
                 findPermutations(nums, curList, result);
-                curList.remove(curList.size() - 1); // 注意！复原操作！！
+                curList.remove(curList.size() - 1); // 复原！
             }
         }                       
     }
@@ -68,11 +67,11 @@ class Solution {
      For example, if the input num[] is {1,2,3}. 
      (i) Add 1 into the initial List<List<Integer>> (let's call it "answer").
      (ii) 2 can be added in front or after 1. So we have to copy the List<Integer> in answer (it's just {1}), then
-     add 2 in position 0 of {1}; then copy the original {1} again, and add 2 in position 1. 
-     Now we have an answer of {{2,1},{1,2}}. There are 2 lists in the current answer.
+         add 2 in position 0 of {1}; then copy the original {1} again, and add 2 in position 1. 
+         Now we have an answer of {{2,1},{1,2}}. There are 2 lists in the current answer.
      (iii) Now we have to add 3. First copy {2,1} and {1,2}, add 3 in position 0; 
-     then copy {2,1} and {1,2}, and add 3 into position 1; 
-     then do the same thing for position 3.
+         then copy {2,1} and {1,2}, and add 3 into position 1; 
+         then do the same thing for position 3.
      Finally we have 2*3=6 lists in answer, which is what we want. */
     public List<List<Integer>> permute(int[] nums) {
       
