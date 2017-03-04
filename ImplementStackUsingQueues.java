684edc16f82b80121e7e73dc58b5079e090efd57
HungryOrc <shu.da.the119@gmail.com>
2017-03-04 00:07:10 -0500

    Update ImplementStackUsingQueues.java

--- ImplementStackUsingQueues.java
+++ ImplementStackUsingQueues.java
@@ -12,8 +12,8 @@ You may simulate a queue by using a list or deque (double-ended queue),
 as long as you use only standard operations of a queue.
 You may assume that all operations are valid (for example, no pop or top operations will be called on an empty stack). */
 
+// 方法1：常驻一个queue，临时性一个queue
 class MyStack {
-    // 必要的 instance variable
     Queue<Integer> fakeStack = new LinkedList<>();
     
     // 把主要的耗时都放在存上，而非取上，能增进用户体验
@@ -40,4 +40,57 @@ class MyStack {
     public boolean empty() {
         return fakeStack.isEmpty();
     }
+}
+
+
+// 方法2：常驻2个queue
+class Stack {
+    
+    Queue<Integer> queue1;
+    Queue<Integer> queue2;
+    
+    public Stack() {
+        queue1 = new LinkedList<>();
+        queue2 = new LinkedList<>();
+    }
+    
+    // Push a new item into the stack
+    public void push(int x) {
+        if (queue1.isEmpty()) {
+            queue1.offer(x);
+            while (!queue2.isEmpty()) {
+                queue1.offer(queue2.poll());
+            }
+        }
+        
+        else { // queue2.isEmpty()
+            queue2.offer(x);
+            while (!queue1.isEmpty()) {
+                queue2.offer(queue1.poll());
+            }
+        }
+    }
+
+    // Pop the top of the stack
+    public void pop() {
+        if (queue2.isEmpty()) {
+            queue1.poll();
+        } else {
+            queue2.poll();
+        }
+    }
+
+    // Return the top of the stack
+    public int top() {
+        if (queue2.isEmpty()) {
+            return queue1.peek();
+        } else {
+            return queue2.peek();
+        }
+    }
+
+    // Check the stack is empty or not.
+    public boolean isEmpty() {
+        return (queue1.isEmpty() && queue2.isEmpty());
+    }    
 }
