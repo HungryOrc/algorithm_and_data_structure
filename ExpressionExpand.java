6b7bef83a9ea0f0d17498d5e1a9f483bc8e23957
HungryOrc <shu.da.the119@gmail.com>
2017-03-03 23:00:18 -0500

    Update ExpressionExpand.java

--- ExpressionExpand.java
+++ ExpressionExpand.java
@@ -73,5 +73,56 @@ public class Solution {
     }
     
     
+    // 方法2：Iteration with Stack
+    // Ref: http://www.jiuzhang.com/solutions/expression-expand/
+    public String expressionExpand(String s) {
+        
+        // 注意！数字和字符和字符串都放在同一个Stack里面 ！！！
+        Stack<Object> stackOfEverything = new Stack<>();
+        int multiplicationOfSubstring = 0;
+        
+        for (char c : s.toCharArray()) {
+            
+            if (c >= '0' && c <= '9') {
+                multiplicationOfSubstring = multiplicationOfSubstring * 10 + c - '0';
+            }
+            
+            else if (c == '[') {
+                stackOfEverything.push(multiplicationOfSubstring);
+                multiplicationOfSubstring = 0;
+            }
+            
+            else if (c == ']') {
+                String substring = popSubstringInOriginalOrder(stackOfEverything);
+                multiplicationOfSubstring = (int)stackOfEverything.pop();
+                
+                for (int i = 1; i <= multiplicationOfSubstring; i++) {
+                    stackOfEverything.push(substring);
+                }
+                multiplicationOfSubstring = 0;
+            }
+            
+            else { // among 'a' to 'z'
+                stackOfEverything.push(String.valueOf(c));
+            }
+        }
+        return popSubstringInOriginalOrder(stackOfEverything);
+    }
+    
+    private String popSubstringInOriginalOrder(Stack<Object> stackOfEverything) {
+        Stack<String> buffer = new Stack<>();
+        
+        // 注意！Object instanceof String ！！！判断是否为一个String的方法！！！
+        // 在这里，原stack停止pop的条件有两种：一种是遇到数字了，一种是本身空了
+        while (!stackOfEverything.isEmpty() && (stackOfEverything.peek() instanceof String)) {
+            buffer.push((String)stackOfEverything.pop());
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        while (!buffer.isEmpty()) {
+            sb.append(buffer.pop());
+        }
+        return sb.toString();
+    }
     
 }
