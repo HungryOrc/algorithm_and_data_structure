ebb921a4c4413a94db63c84176ce9f5dd7819051
HungryOrc <shu.da.the119@gmail.com>
2017-03-06 23:20:56 -0500

    Create TernaryExpressionToBinaryTree.java

---
+++ TernaryExpressionToBinaryTree.java
@@ -0,0 +1,30 @@
+/* Ref: https://discuss.leetcode.com/topic/28256/ternary-expression-to-binary-tree
+Convert a ternary expression to a binary tree.
+Assume that all these ternary expression are valid, 
+connamely they won't cause any error or conflict when building the binary trees accordingly.
+
+Examples: 
+a?b:c to
+  a
+ /  \
+b   c
+
+a?b?c:d:e to
+     a
+    / \
+   b   e
+  / \
+ c   d
+
+a?b:c?d:e
+  a
+ / \
+b   c
+   / \
+  d   e             */
+ 
+class TreeNode {
+
+
+
+}
