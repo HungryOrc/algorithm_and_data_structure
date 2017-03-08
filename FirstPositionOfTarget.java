4d68b126d889dacc9b3af395deee6dec1157aed2
HungryOrc <shu.da.the119@gmail.com>
2017-03-08 10:17:58 -0500

    Update FirstPositionOfTarget.java

--- FirstPositionOfTarget.java
+++ FirstPositionOfTarget.java
@@ -5,50 +5,36 @@ Example
 If the array is [1, 2, 3, 3, 4, 5, 10], for given target 3, return 2. */
 
 public class Solution {
-    /**
-     * @param nums: An integer array sorted in ascending order
-     * @param target: An integer
-     * @return an integer
-     */
-    public int findFirstPosition(int[] nums, int target) {
+
+    // Jiu Zhang binary search template
+    public int binarySearch(int[] nums, int target) {
         
         if (nums == null || nums.length == 0) {
             return -1;
         }
         
-        int len = nums.length;
-        int start = 0, end = len - 1;
+        int start = 0, end = nums.length - 1;
         
-        while (start < end) {
+        while (start + 1 < end) {
+            
             int mid = start + (end - start) / 2;
             
             if (nums[mid] == target) {
-                if (mid == 0) {
-                    return 0;
-                }
-                else {
-                    if (nums[mid - 1] == target) {
-                        end = mid - 1;
-                    }
-                    else {
-                        return mid;
-                    }
-                }
-            }
-            else if (nums[mid] < target) {
+                end = mid;
+            } else if (nums[mid] < target) {
                 start = mid + 1;
-            }
-            else {
+            } else { // > target
                 end = mid - 1;
             }
         }
         
-        // 此时 start == end
+        // 因为要找first position，而非last position，
+        // 所以先查验start，再查验end。否则就反过来了
         if (nums[start] == target) {
             return start;
+        } else if (nums[end] == target) {
+            return end;
         }
-        else {
-            return -1;
-        }
+        return -1;
     }
 }
