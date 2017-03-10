12063eced3bd4310b125b0f7dea29dc51794fa13
HungryOrc <shu.da.the119@gmail.com>
2017-03-10 12:45:30 -0500

    Update MultipleIntersectionsOfTwoArrays.java

--- MultipleIntersectionsOfTwoArrays.java
+++ MultipleIntersectionsOfTwoArrays.java
@@ -6,63 +6,62 @@ Note:
 
 public class Solution {
     
-    // 方法1：用 HashSet
-    // Time: O(n)
+    // 方法1：Two HashSets. Time: O(n)
+    // 不要用 一个HashSet 加上 一个ArrayList！！！那样太慢！！！
     public int[] intersection(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums2 == null) {
+            return null;    
+        }
         
-        HashSet<Integer> numsInArrayOne = new HashSet<>();
-        ArrayList<Integer> output_ArrayList = new ArrayList<>();
+        HashSet<Integer> numsIn1 = new HashSet<>();
+        HashSet<Integer> intersections = new HashSet<>();
         
-        for (int num : nums1)
-            // 注意！HashSet 是 add 不是 put！！
-            numsInArrayOne.add(num);
+        for (int num : nums1) {
+            numsIn1.add(num);
+        }
         
-        for (int num : nums2)
-        {
-            if (numsInArrayOne.contains(num))
-            {
-                // 注意！HashSet 的 contains
-                output_ArrayList.add(num);
-                // 要把查出来是重复的数从 HashSet 里删掉！！
-                numsInArrayOne.remove(num);
+        for (int num : nums2) {
+            if (numsIn1.contains(num) && !intersections.contains(num)) { 
+                intersections.add(num);
             }
         }
-        int[]output_Array = new int[output_ArrayList.size()];
-        for (Integer num : output_ArrayList)
-            output_Array[i] = output_ArrayList.get(i);
-    
-        return output_Array;
+        
+        int[] result = new int[intersections.size()];
+        int i = 0;
+        for (int num : intersections) {
+            result[i] = num;
+            i++;
+        }
+        return result;
     }
     
     
-    // 方法2：Sort both Arrays, use 2 pointers from head and tail respectively
+    // 方法2：Sort both Arrays, use 2 pointers for 2 arrays respectively, both of them go from left to right
     // Ref: https://discuss.leetcode.com/topic/45685/three-java-solutions
     // Time: O(n*log(n)). 后面处理重复数的过程只用O(n)，但前面排序数组用了O(n*log(n))
-    public int[] intersection(int[] num1, int[] num2)
-    {
+    public int[] intersection(int[] num1, int[] num2) {
         HashSet<Integer> interNums = new HashSet<>();
         Arrays.sort(nums1);
         Arrays.sort(nums2);
+        
         int i = 0, j = 0;
-        while (i < nums1.length && j < nums2.length)
-        {
+        while (i < nums1.length && j < nums2.length) {
             if (nums1[i] < nums2[j])
                 i++;
             else if (nums2[j] < nums1[i])
                 j++;
-            else // nums1[i] == nums2[j]
-            {
+            else { // nums1[i] == nums2[j]
                 interNums.add(nums1[i]);
                 i ++;
                 j ++;
             }
         }
-        int[] output = new int[interNums.size()];
-        int k = 0;
-        // 注意！HashSet 也可以用这种 Enhanced For Loop ！！
+        
+        int[] result = new int[interNums.size()];
+        int i = 0;
         for (Integer num : interNums)
-            output[k ++] = num;
-        return output;
+            result[i++] = num;
+        return result;
     }
     
     
@@ -70,37 +69,29 @@ public class Solution {
     // Ref: https://discuss.leetcode.com/topic/45685/three-java-solutions
     // Time: O(n*log(n)). 后面处理重复数的过程只用O(log(n))，但前面排序数组用了O(n*log(n))
     public int[] intersection(int[] nums1, int[] nums2) {
-        
         HashSet<Integer> interNums = new HashSet<>();
         Arrays.sort(nums2);
-        for (Integer num : nums1)
-        {
+        // 这个方法，只用排列2个数列中的一个即可。被排列的这个要用于binary search
+        
+        for (Integer num : nums1) {
             if (binarySearch(nums2, num))
                 interNums.add(num);
         }
+        
         int i = 0;
-        int[] output = new int[interNums.size()];
-        for (Integer num : interNums)
-        {
-            output[i] = num;
+        int[] result = new int[interNums.size()];
+        for (Integer num : interNums) {
+            result[i] = num;
             i++;
         }
-        return output;
+        return result;
     }
     
-    public boolean binarySearch(int[] nums, int target)
-    {
+    private boolean binarySearch(int[] nums, int target) {
         int low = 0;
         int high = nums.length - 1;
-        while (low <= high)
-        {
-            /* 注意 mid ！！！
-             如果包含low和high在内共有奇数个数，则mid是正中间那个
-             如果共有偶数个数，则mid是中间偏小那个
-             如果共有2个数，则mid = low
-            */
+        while (low <= high) {
             int mid = low + (high - low) / 2;
-            
             if (nums[mid] == target)
                 return true;
             else if (nums[mid] > target)
@@ -110,6 +101,5 @@ public class Solution {
         }
         return false;
     }
-    
     
 }
