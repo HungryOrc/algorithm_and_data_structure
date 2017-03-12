73d1d96d142e0a200c4065a76473bbcaf2b9b1b0
HungryOrc <shu.da.the119@gmail.com>
2017-03-11 21:44:26 -0500

    Update BinaryTreePathSum_Existence.java

--- BinaryTreePathSum_Existence.java
+++ BinaryTreePathSum_Existence.java
@@ -24,25 +24,22 @@ public class Solution {
     
     // 方法1: Recursion
     public boolean hasPathSum(TreeNode root, int sum) {
-        
         if (root == null)
             return false;
         
         // 到当前节点为止，正好加和为sum。而且当前节点正好是一个leaf
         if (root.val == sum && root.left == null && root.right == null)
             return true;
-        else
-        {
+        else {
             sum -= root.val;
             return (hasPathSum(root.left, sum) || hasPathSum(root.right, sum));
         }
     }
     
     
-    // 方法2: Iteration, by Stack, DFS
+    // 方法2: Iteration, by 2 Stacks, DFS
     // 如果用Queue做BFS，也是大同小异
     public boolean hasPathSum(TreeNode root, int sum) {
-        
         if (root == null)
             return false;
         
@@ -51,8 +48,7 @@ public class Solution {
         Stack<Integer> sumStack = new Stack<>();
         sumStack.push(0);
         
-        while(!nodeStack.isEmpty())
-        {
+        while(!nodeStack.isEmpty()) {
             TreeNode curNode = nodeStack.pop();
             int curSum = sumStack.pop();
             curSum += curNode.val;
@@ -60,19 +56,16 @@ public class Solution {
             if (curSum == sum && curNode.left == null && curNode.right == null)
                 return true;
             
-            if (curNode.left != null)
-            {
+            if (curNode.left != null) {
                 nodeStack.push(curNode.left);
                 sumStack.push(curSum);
             }
-            if (curNode.right != null)
-            {
+            if (curNode.right != null) {
                 nodeStack.push(curNode.right);
                 sumStack.push(curSum);
             }
         }
         return false;
     }
-    
     
 }
