4b2afb4a56ce75fece5c0dc0dfa472a8266189f0
HungryOrc <shu.da.the119@gmail.com>
2017-03-15 23:19:00 -0400

    Update BinaryTreeMaxPathSum_FromRootToAnyNode.java

--- BinaryTreeMaxPathSum_FromRootToAnyNode.java
+++ BinaryTreeMaxPathSum_FromRootToAnyNode.java
@@ -19,9 +19,9 @@ return 4. (1->3)
  *     }
  * } */
 
+// 方法1：Recursion - Divide and Conquer
 public class Solution {
 
-    // 方法1：Recursion - Divide and Conquer
     public int maxPathSum2(TreeNode root) {
         if (root == null) {
             return 0;
@@ -36,14 +36,67 @@ public class Solution {
         } else { // 如果 <= 0，那么还不如不加下面的子树了，就只有root.val自己反而更大
             return root.val;
         }
+    } 
+}
+
+
+// 方法2：Recursion - Traversal
+public class Solution {
+    int maxPathSum;
+    
+    public int maxPathSum2(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        maxPathSum = Integer.MIN_VALUE;
+        
+        findMaxPathSumFromRootToAnyNode(root, root.val);
+        return maxPathSum;
     }
     
-    
-    
-    
-    
-    
-    
-    
-    
+    private void findMaxPathSumFromRootToAnyNode(TreeNode curNode, int curPathSum) {
+        maxPathSum = Math.max(maxPathSum, curPathSum);
+        
+        if (curNode.left != null) {
+            findMaxPathSumFromRootToAnyNode(curNode.left, curPathSum + curNode.left.val);
+        }
+        if (curNode.right != null) {
+            findMaxPathSumFromRootToAnyNode(curNode.right, curPathSum + curNode.right.val);
+        }
+    }
+}
+
+
+// 方法3：Iteration (即 Non-Recursion)。用2个Stack。或者用2个Queue也是一样的，这个在这里无关紧要
+public class Solution {
+
+    public int maxPathSum2(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        
+        int maxPathSum = Integer.MIN_VALUE;
+        
+        Stack<TreeNode> nodeStack = new Stack<>();
+        nodeStack.push(root);
+        Stack<Integer> pathSumStack = new Stack<>();
+        pathSumStack.push(root.val);
+        
+        while (!nodeStack.isEmpty()) {
+            TreeNode curNode = nodeStack.pop();
+            int curPathSum = pathSumStack.pop();
+            
+            maxPathSum = Math.max(maxPathSum, curPathSum);
+            
+            if (curNode.left != null) {
+                nodeStack.push(curNode.left);
+                pathSumStack.push(curPathSum + curNode.left.val);
+            }
+            if (curNode.right != null) {
+                nodeStack.push(curNode.right);
+                pathSumStack.push(curPathSum + curNode.right.val);
+            }
+        }
+        return maxPathSum;
+    }
 }
