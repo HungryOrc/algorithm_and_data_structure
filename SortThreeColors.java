ce56b1aa32945a93a9c240aac013e29d982f2af5
HungryOrc <shu.da.the119@gmail.com>
2017-03-27 10:20:46 -0400

    Update SortThreeColors.java

--- SortThreeColors.java
+++ SortThreeColors.java
@@ -75,45 +75,47 @@ class Solution {
 
 // 方法2：one pass
 // Ref: http://www.jiuzhang.com/solutions/sort-colors/
-/* 很巧妙！三个pointer。诀窍在于：
-   left左边，不含left，保证一定是0
-   right右边，不含right，保证一定是2
-   index左边，不含index，保证一定是0或1 */
+/* 很巧妙！三个隔板：
+   redDivider的左边，不含它自己，保证一定是0
+   blueDivider的右边，不含它自己，保证一定是2
+   whiteDivider的左边，不含它自己，保证一定是0或1。
+   whiteDivider如果与redDivider不重合，则whiteDivider的左边（不含它自己）到redDivier的右边（含它自己）一定是1
+   whiteDivider的右边（含它自己）到blueDivider的左边（含它自己）的区域可能含有0,1,2中的任何值 */
    
 class Solution {
-    
     int RED = 0;
     int WHITE = 1;
     int BLUE = 2;
     
     public void sortColors(int[] nums) {
-        
         if (nums == null || nums.length <= 1) {
             return;
         }
         
-        int left = 0, right = nums.length - 1;
+        int divider0 = 0; // red divider
+        int divider1 = 0; // white divider
+        int divider2 = nums.length - 1; // blue divider
         
-        int index = 0;
-        
-        // 要一直做到 = ！不能停止于 < ！
+        // 要一直做到 = ！不能停止于 < ！否则那一个位置就没有被处理！
         while (index <= right) {
             
-            if (nums[index] == RED) {
-                swap(nums, index, left);
-                // 这里index要++，因为index的左边(不含index)一定要么是0要么是1！！！
-                index ++;
-                left ++;
+            if (nums[divider1] == RED) {
+                swap(nums, divider1, divider0);
+                divider0 ++;
+                // 这里divider1可以跟着divider0一起都 ++，是因为这题里面只有3种颜色，
+                // 从divider0那里swap过来的颜色，一定是1，只可能是1，1也就是divider1要隔到它左边去的颜色
+                divider1 ++;
             }
             
-            else if (nums[index] == WHITE) {
-                index ++;
+            else if (nums[divider1] == WHITE) {
+                divider1 ++;
             }
             
             else { // == BLUE
-                swap(nums, index, right);
-                right --;
-                // 这里不要 index ++ 了！！！下一回合还要看从right调换过来的是什么！！！
+                swap(nums, divider1, divider2);
+                divider2 --;
+                // 这里不要 divider1 ++ 了！！！因为现在还不能知道这次从divider2调换过来的是什么颜色！！！
+                // 必须留待下一个loop才能查到！！！
             }
         }
     }
