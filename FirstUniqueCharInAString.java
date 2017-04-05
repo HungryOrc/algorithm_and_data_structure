e9006674aaa1ab7ae759561a6c0f2987d4f07f13
HungryOrc <shu.da.the119@gmail.com>
2017-04-04 20:15:12 -0400

    Update FirstUniqueCharInAString.java

--- FirstUniqueCharInAString.java
+++ FirstUniqueCharInAString.java
@@ -43,11 +43,10 @@ public class Solution {
     public int firstUniqChar (String s)
     {
         int[] freq = new int[26];
-        // ！String可以直接求长度！String.length() ！
-        for (int i = 0; i < s.length(); i++)
+        for (int i = 0; i < s.length(); i++) {
             freq[s.charAt(i) - 'a'] ++;
-        for (int i = 0; i < s.length(); i++)
-        {
+        }
+        for (int i = 0; i < s.length(); i++) {
             if (freq[s.charAt(i) - 'a'] == 1)
                 return i;
         }
