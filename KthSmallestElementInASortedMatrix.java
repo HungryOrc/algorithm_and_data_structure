0e7753f2dd6eb68c1e25b39536f31958212a2770
HungryOrc <shu.da.the119@gmail.com>
2017-04-19 00:23:57 -0400

    Update KthSmallestElementInASortedMatrix.java

--- KthSmallestElementInASortedMatrix.java
+++ KthSmallestElementInASortedMatrix.java
@@ -27,7 +27,68 @@ Note: You may assume k is always valid, 1 ≤ k ≤ n^2.
 上面第一排最右边的5，就比第二排第一个的6要小。 */
 
 
-// 方法2：用Binary Search做。我没有想到。很厉害
+// 方法1：用 Priority Queue + Custom Class 来做。
+// 注意！！！这里既不是 先进先出，也不是 后进先出！！！既不是 BFS，也不是 DFS ！！！而是 “谁最小谁先出” ！！！
+// 然后再把最小的cell周围的2个cell放到 priority queue 里面去 ！！！然后再看下一个最小是谁 ！！！
+// 这里 每个cell的坐标和value值都要封装到 cell class 里面去，作为一个整体塞到 priority queue 里 ！！！
+class Cell {
+  int x, y;
+  int value;
+  
+  public Cell(int x, int y, int value) {
+    this.x = x;
+    this.y = y;
+    this.value = value;
+  }
+}
+
+public class Solution {
+  
+  public int kthSmallest(int[][] matrix, int k) {
+    int rows = matrix.length;
+    int cols = matrix[0].length;
+    
+    PriorityQueue<Cell> minValueCells = new PriorityQueue<Cell>(k, new Comparator<Cell>() {
+      @Override
+      public int compare(Cell c1, Cell c2) {
+        if (c1.value == c2.value) {
+          return 0;
+        }
+        return c1.value > c2.value ? 1 : -1;
+      }
+    });
+      
+    minValueCells.offer(new Cell(0, 0, matrix[0][0]));
+    
+    boolean visited[][] = new boolean[rows][cols];
+    visited[0][0] = true;
+    
+    // offer into the min heap for additional k-1 times
+    for (int count = 1; count <= k - 1; count++) {
+      
+      // get the Cell with the current minimum value
+      Cell curMinCell = minValueCells.poll();
+      
+      int curX = curMinCell.x;
+      int curY = curMinCell.y;
+      int curValue = curMinCell.value;
+      
+      if (curX < rows - 1 && visited[curX + 1][curY] == false) {
+        minValueCells.offer(new Cell(curX + 1, curY, matrix[curX + 1][curY]));
+        visited[curX + 1][curY] = true;
+      }
+      if (curY < cols - 1 && visited[curX][curY + 1] == false) {
+        minValueCells.offer(new Cell(curX, curY + 1, matrix[curX][curY + 1]));
+        visited[curX][curY + 1] = true;
+      }
+    }
+    
+    return minValueCells.peek().value;
+  }  
+}
+
+
+// 方法2：用Binary Search做。我没有想到。很巧妙 ！！！
 // Ref: https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/
 public class Solution 
 {
