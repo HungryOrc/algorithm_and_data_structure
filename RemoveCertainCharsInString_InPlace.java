1b60dff8acd1fee82233819de7a6c902c24ba3ae
HungryOrc <shu.da.the119@gmail.com>
2017-04-22 19:38:10 -0400

    Create RemoveCertainCharsInString_InPlace.java

---
+++ RemoveCertainCharsInString_InPlace.java
@@ -0,0 +1,35 @@
+/* Remove given characters in input string, the relative order of other characters should be remained. 
+Return the new string after deletion.
+
+Assumptions
+The given input string is not null.
+The characters to be removed is given by another string, it is guranteed to be not null.
+
+Examples
+input = "abcd", t = "ab", delete all instances of 'a' and 'b', the result is "cd". */
+
+// 要求 in place
+public class Solution {
+  
+  public String remove(String input, String t) {
+    
+    HashSet<Character> toBeRemoved = new HashSet<>();
+    for (char c : t.toCharArray()) {
+      toBeRemoved.add(c);
+    }
+    
+    char[] cArray = input.toCharArray();
+    int index = 0; // 相当于两个指针一快一慢的做法
+    
+    for (int i = 0; i < input.length(); i++) {
+      if (!toBeRemoved.contains(input.charAt(i))) {
+        cArray[index] = cArray[i];
+        index ++;
+      }
+    }
+    
+    // index在最后还会自增1，所以这里的上限用index即可
+    return new String(cArray, 0, index);
+  }
+  
+}
