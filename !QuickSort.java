4bcc3225af5668668a152a6ea5602e4c78724dda
HungryOrc <shu.da.the119@gmail.com>
2017-04-22 22:27:38 -0400

    Update !QuickSort.java

--- !QuickSort.java
+++ !QuickSort.java
@@ -1,54 +1,7 @@
-// 九章模板
-public class Solution {
-
-    public void sortIntArray(int[] nums) {
-        quickSort(nums, 0, nums.length - 1);
-    }
-    
-    private void quickSort(int[] nums, int start, int end) {
-        if (start >= end) {
-            return;
-        }
-        
-        int left = start, right = end;
-        int pivot = nums[start + (end - start) / 2];
-
-        // 注意 ！！！ every time you compare left & right, it should be left <= right, not left < right
-        // 在整个程序中的所有地方，只要是比较left和right，都要遵循这个原则 ！！！
-        while (left <= right) {
-            
-            // 注意 ！！！ nums[left] < pivot, not nums[left] <= pivot
-            while (left <= right && nums[left] < pivot) {
-                left++;
-            }
-            
-            // 注意 ！！！ nums[right] > pivot, not nums[right] >= pivot
-            while (left <= right && nums[right] > pivot) {
-                right--;
-            }
-            
-            if (left <= right) {
-                int temp = nums[left];
-                nums[left] = nums[right];
-                nums[right] = temp;
-                
-                // 注意 ！！！ 在这里也要 left++ 以及 right-- ！！！
-                left++;
-                right--;
-            }
-        }
-        // 特别注意 ！！！
-        // 这个 while 循环结束以后，left 和 right 的关系是：
-        // right + 1 = left ！！！ 即 right 在 left 的左边一位 ！！！
-        
-        // 这里没有必要判断左右index的大小关系，下一个recursion里的头部会做。当然如果一定要在这里做，也ok
-        quickSort(nums, start, right);
-        quickSort(nums, left, end);
-    }
-}
-
-
 // 经典模板
+// 时间：O(n*logn)
+// 空间：O(logn)。因为一共有logn层 call stack，每一个 call stack 都耗费constant空间，
+// 它们都没有创造额外的helper array之类的空间占用者
 public class QuickSort {
     
     public int[] quickSort(int[] array) {
@@ -131,5 +84,55 @@ public class QuickSort {
         int temp = array[left];
         array[left] = array[right];
         array[right] = temp;
+    }
+}
+
+
+// 九章模板
+public class Solution {
+
+    public void sortIntArray(int[] nums) {
+        quickSort(nums, 0, nums.length - 1);
+    }
+    
+    private void quickSort(int[] nums, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+        
+        int left = start, right = end;
+        int pivot = nums[start + (end - start) / 2];
+
+        // 注意 ！！！ every time you compare left & right, it should be left <= right, not left < right
+        // 在整个程序中的所有地方，只要是比较left和right，都要遵循这个原则 ！！！
+        while (left <= right) {
+            
+            // 注意 ！！！ nums[left] < pivot, not nums[left] <= pivot
+            while (left <= right && nums[left] < pivot) {
+                left++;
+            }
+            
+            // 注意 ！！！ nums[right] > pivot, not nums[right] >= pivot
+            while (left <= right && nums[right] > pivot) {
+                right--;
+            }
+            
+            if (left <= right) {
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                
+                // 注意 ！！！ 在这里也要 left++ 以及 right-- ！！！
+                left++;
+                right--;
+            }
+        }
+        // 特别注意 ！！！
+        // 这个 while 循环结束以后，left 和 right 的关系是：
+        // right + 1 = left ！！！ 即 right 在 left 的左边一位 ！！！
+        
+        // 这里没有必要判断左右index的大小关系，下一个recursion里的头部会做。当然如果一定要在这里做，也ok
+        quickSort(nums, start, right);
+        quickSort(nums, left, end);
     }
 }
