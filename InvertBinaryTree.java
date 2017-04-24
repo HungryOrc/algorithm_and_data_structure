1c799c681b06db2a6fb132397a816656fe90c074
HungryOrc <shu.da.the119@gmail.com>
2017-04-24 09:31:32 -0400

    Update InvertBinaryTree.java

--- InvertBinaryTree.java
+++ InvertBinaryTree.java
@@ -30,61 +30,64 @@ public class Solution {
      you can find the problem size that will OVERFLOW the STACK and crash your application. 
      So more robust solution would be to use Stack data structure, as we show later.
      */
-     public TreeNode invertTree_RecursiveDFS (TreeNode root) 
-     {
+     public TreeNode invertTree_RecursiveDFS (TreeNode root)  {
           if (root == null)
-               return null;
+              return null;
+          
           TreeNode formerLeft = root.left;
           TreeNode formerRight = root.right;
           root.left = invertTree_RecursiveDFS(formerRight);
           root.right = invertTree_RecursiveDFS(formerLeft);
+          
           return root;
      }
      
      
      // Iterative DFS
-     public TreeNode invertTree_IterativeDFS (TreeNode root)
-     {
+     public TreeNode invertTree_IterativeDFS (TreeNode root) {
           if (root == null)
-               return null;
+              return null;
+          
           Stack<TreeNode> nodeStack = new Stack<TreeNode>();
           nodeStack.push(root);
-          while (!nodeStack.isEmpty())
-          {
+          
+          while (!nodeStack.isEmpty()) {
                TreeNode curNode = nodeStack.pop();
+               // 接下来的三步就是swap
                TreeNode formerLeft = curNode.left;
                curNode.left = curNode.right;
                curNode.right = formerLeft;
                
                if (curNode.left != null)
-                    nodeStack.push(curNode.left);
+                   nodeStack.push(curNode.left);
                if (curNode.right != null)
-                    nodeStack.push(curNode.right);
+                   nodeStack.push(curNode.right);
           }
           return root;
      }
 
      
      // Iterative BFS
-     public TreeNode invertTree_IterativeBFS (TreeNode root)
-     {
+     public TreeNode invertTree_IterativeBFS (TreeNode root) {
           if (root == null)
-               return null;
+              return null;
+          
           Queue<TreeNode> nodeQueue = new LinkedList<TreeNode>();
           nodeQueue.offer(root);
-          while (!nodeQueue.isEmpty())
-          {
+          
+          while (!nodeQueue.isEmpty()) {
                TreeNode curNode = nodeQueue.poll();
+               // 接下来的三步就是swap
                TreeNode formerLeft = curNode.left;
                curNode.left = curNode.right;
                curNode.right = formerLeft;
                
                if (curNode.left != null)
-                    nodeQueue.offer(curNode.left);
+                   nodeQueue.offer(curNode.left);
                if (curNode.right != null)
-                    nodeQueue.offer(curNode.right);
+                   nodeQueue.offer(curNode.right);
           }
           return root;
      }
-      
+
 }
