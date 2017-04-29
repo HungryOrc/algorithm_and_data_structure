6d2611d19dbb6616e5fc2be4be05e951a09e64c2
HungryOrc <shu.da.the119@gmail.com>
2017-04-29 19:47:08 -0400

    Create !QueueAndList.java

---
+++ !QueueAndList.java
@@ -0,0 +1,16 @@
+/* 各种 Queue 和 List 的最佳实现方式：
+
+// Queue:
+Queue<XXX> myQueue = new LinkedList<>();
+
+// Stack:
+Dequeue<XXX> myStack = new LinkedList<>();
+
+// 2-End Queue:
+Dequeue<XXX> myDequeue = new LinkedList<>();
+
+// Size-Adjustable Array:
+List<XXX> myList = new ArrayList<>();
+
+// Priority Queue:
+Queue<XXX> myPQ = new PriorityQueue<>(); */
