67cf740f1d198c0eb7d5e0350497e9550053c3a1
HungryOrc <shu.da.the119@gmail.com>
2017-04-29 11:36:27 -0400

    Update GetKeysInBinarySearchTreeInGivenRange.java

--- GetKeysInBinarySearchTreeInGivenRange.java
+++ GetKeysInBinarySearchTreeInGivenRange.java
@@ -1,4 +1,4 @@
-/* Get the list of keys in a given binary search tree in a given range[min, max] in ascending order, both min and max are inclusive.
+/* Get the list of keys in a given binary search tree in a given range[k1, k2] in ascending order, both min and max are inclusive.
 Corner Cases: What if there are no keys in the given range? Return an empty list in this case.
 
 Examples:
@@ -18,8 +18,19 @@ get the keys in [2, 5] in ascending order, result is  [3, 4, 5]
 *   }
 * } */
 
-// 对于BST，我们知道通过 in-order traversal 可以获得所有节点从小到大的排布。那我们就从这里入手，
-// 做一个 “每一步都经过判断的，局部的 in-order traversal”
+/* Laioffer 的方法。对于BST，我们知道通过 in-order traversal 可以获得所有节点从小到大的排布。那我们就从这里入手，
+做一个 “每一步都经过判断的，局部的 in-order traversal”
+
+时间：O(logn + |k2 - k1|)
+解释：这里的处理逻辑，其实可以认为是由2部分构成的：
+(1) 在 BST 里查找 k1 和 k2
+(2) 遍历 k2 - k1 的部分
+而前面部分的开销和后面部分的开销，在不同情况下，不一定谁dominate。
+比如刚好 k2 - k1 的部分没有任何一个node，这个时候前面的logn会dominate。如果刚好k2 - k1的部分包含了所有的node，那么这部分会dominate。
+所以正确的复杂度是 logn + |k2 - k1|。
+        
+空间：O(logn)，因为一共有这么多层的 call stack */
+
 public class Solution {
   
   public List<Integer> getRange(TreeNode root, int min, int max) {
