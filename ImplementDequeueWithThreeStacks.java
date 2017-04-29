7f5bcdf37a57f592b46e4760ea32cc2caca75563
HungryOrc <shu.da.the119@gmail.com>
2017-04-28 21:28:10 -0400

    Update ImplementDequeueWithThreeStacks.java

--- ImplementDequeueWithThreeStacks.java
+++ ImplementDequeueWithThreeStacks.java
@@ -1,3 +1,23 @@
+/* 用多个Stack来实现一个自定义的Dequeue
+最直观来说，用2个Stack尾对尾（栈底贴栈底，两个栈顶口向外）就可以实现，如下图：
+
+        leftStack          rightStack
+    topL      bottomL   bottomR    topR
+    7    6    5    4  ||  3    2    1  
+dequeueHead                        dequeueTail
+
+这样的设计，push都没问题，pop的时候只要上面2个Stack不空也没问题；
+问题就是如果pop的时候被pop的那一端的那个stack已经空了，比如pop head，结果left stack已经空了，
+就得从另一个stack即right stack把所有元素倒到left stack，再pop掉最上面那个元素，再把剩下的元素都倒回到right stack去。
+这样，一头空了以后，对这一头的每一次的pop都一定是n的时间复杂度，太慢了。
+
+改进方式是，再加上第三个stack做buffer。每次一头空了以后，就从另一头把一半的元素挪过来。关键是要保持“两头”的相对顺序。如下图：
+         leftStack      rightStack      bufferStack
+       7 6 5 4 3 2 1 || empty       
+   =>          3 2 1 || empty            4 5 6 7 ||
+   =>          empty || 3 2 1            4 5 6 7 ||
+   =>        7 6 5 4 || 3 2 1                                         */
+
 import java.util.Stack;
 
 public class DequeueByStacks {
