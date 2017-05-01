829d4cae22f5e25aa32584f1628d550f01cfb9f4
HungryOrc <shu.da.the119@gmail.com>
2017-05-01 00:51:02 -0400

    Update StockTicker.java

--- StockTicker.java
+++ StockTicker.java
@@ -1,52 +1,53 @@
 /* Give a stream of stock prices, design a data structure to support the following operations:
-
-StockSticker(int k) Initialize the size of the ticker.
-void addOrUpdate(String stock, double price) add or update the price of a stock to the data structure.
-List<Map.Entry<String, double>> top() return the top k price stocks and their current prices. */
+(1) StockSticker(int k):
+Initialize the size of the ticker.
+(2) void addOrUpdate(String stock, double price):
+add or update the price of a stock to the data structure.
+(3) List<Map.Entry<String, double>> top(): 
+return the top k price stocks and their current prices. */
 
 /* 思路：
 update price -O(log(n)), red black tree is used
 get top(k) - O(k) */
 
 public class StockTicker {	    
-  private int k;
-  private HashMap<String, Double> map;
-  private TreeSet<Map.Entry<String, Double>> set;
+    private int k;
+    private HashMap<String, Double> map; // <name, price>
+    private TreeSet<Map.Entry<String, Double>> treeSet;
 
-  public StockTicker(int k) {
-    this.k = k;
-    map = new HashMap<String,Double>();
-    set = new TreeSet<> (new Comparator<Map.Entry<String, Double>>() {
-    @Override
-    public int compare(
-        java.util.Map.Entry<String, Double> obj1,
-        java.util.Map.Entry<String, Double> obj2) {
-      int res = obj2.getValue().compareTo(obj1.getValue());
-      if (res == 0)
-        return obj2.getKey().compareTo(obj1.getKey());	
-      return res;
+    public StockTicker(int k) {
+        this.k = k;
+        map = new HashMap<>();
+      
+        treeSet = new TreeSet<> (new Comparator<Map.Entry<String, Double>>() {
+            @Override
+            public int compare(Map.Entry<String, Double> obj1, Map.Entry<String, Double> obj2) {
+                int res = obj2.getValue().compareTo(obj1.getValue());
+                if (res == 0) { // 如果 price 相同，再比较 股票名称
+                    return obj2.getKey().compareTo(obj1.getKey());
+                }
+                return res;
+            }
+        });
     }
 
-    });
-  }
-
-  public void addOrUpdate(String stock,  double price)  {
-    AbstractMap.SimpleEntry<String, Double> entry =  new AbstractMap.SimpleEntry<>(stock,  price);	
-    if (map.containsKey(stock)) {
-        set.remove(new AbstractMap.SimpleEntry<>(stock, map.get(stock)));	    				
+    public void addOrUpdate(String stock,  double price)  {
+        AbstractMap.SimpleEntry<String, Double> entry =  new AbstractMap.SimpleEntry<>(stock, price);	
+        if (map.containsKey(stock)) {
+            treeSet.remove(new AbstractMap.SimpleEntry<>(stock, map.get(stock)));	    				
+        }
+        map.put(stock, price);
+        treeSet.add(entry);     		
     }
-    map.put(stock, price);
-    set.add(entry);     		
-  }
 
-  public List<Map.Entry<String, Double>> top() {
-    List<Map.Entry<String, Double>> ls = new ArrayList<>();
-    int i = 0;
-    Iterator<Map.Entry<String, Double>> setIterator = set.iterator();
-    while (i  <  k && setIterator.hasNext()) {
-      ls.add(setIterator.next());
-      i++;
+    public List<Map.Entry<String, Double>> top() {
+        List<Map.Entry<String, Double>> ls = new ArrayList<>();
+        int i = 0;
+        Iterator<Map.Entry<String, Double>> setIterator = treeSet.iterator();
+        while (i < k && setIterator.hasNext()) {
+            ls.add(setIterator.next());
+            i++;
+        }
+        return ls;
     }
-    return ls;
-  }
 }
