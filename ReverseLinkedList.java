b7d30f8a3805d6ba7d8c5cfa786165bcff3df089
HungryOrc <shu.da.the119@gmail.com>
2017-05-06 14:07:02 -0400

    Update ReverseLinkedList.java

--- ReverseLinkedList.java
+++ ReverseLinkedList.java
@@ -42,11 +42,23 @@ public class Solution {
      
         // 第二步：经过处理后的后部，与当前的前部，应该发生哪些关系？一个也不要漏！
         nextNode.next = head;
+        
         /* 特别注意下面这句 ！！！
-        如果是第一个head node，这么做就完事儿了。它也就该next -> null 
-        但是对于原head以后的任何nodes，把它们的next设为null怎么行呢？！！窍门在于：
-        reverseList这个函数在上面是不断嵌套自己的，
-        在里面的一层，把某个node的next设为null以后；在紧邻着的外面的一层，就会再把它的next设为它之前的prev */
+          如果是第一个head node，这么做就完事儿了。它也就该next -> null 
+          但是对于原head以后的任何nodes，把它们的next设为null怎么行呢？！！窍门在于：
+          reverseList这个函数在上面是不断嵌套自己的，
+          在里面的一层，把某个node的next设为null以后；在紧邻着的外面的一层，就会再把它的next设为它之前的prev 
+         
+          从另外一个角度理解这个问题：对于head来说，sub problem是：
+            1 -> 2 -> 3 -> 4 -> null
+                \___sub problem ___/
+              
+          sub problem搞定以后，它的结果其实是这个样子：注意！节点2 在此时是指向null的 ！！！
+            1 -> 2 <- 3 <- 4
+                 |
+                 V
+                null                                                                       */
+     
         head.next = null;
      
         return newHead;
