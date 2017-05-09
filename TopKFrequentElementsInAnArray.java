f9277e6986d1c930693ce2f71134384cc0ec740f
HungryOrc <shu.da.the119@gmail.com>
2017-05-08 21:56:50 -0400

    Update TopKFrequentElementsInAnArray.java

--- TopKFrequentElementsInAnArray.java
+++ TopKFrequentElementsInAnArray.java
@@ -50,45 +50,39 @@ public class Solution
     }
     
     
-    // 方法2：我的朴素方法，用ArrayList装MapEntry，然后再用 custom comparator 来 sort ArrayList，
-    // 虽然思路很原始，数据结构也很直觉很简单，但速度还不错
-    public List<Integer> topKFrequent(int[] nums, int k) 
-    {
-        HashMap<Integer,Integer> numCountsMap = new HashMap<>();
-        for (int n : nums) {
-            numCountsMap.put(n, numCountsMap.getOrDefault(n, 0) + 1);
-        }
+    // 方法2：TreeMap. 高大上哈哈！
+    // Use freqncy as the key so we can get all freqencies in order
+    public class Solution {
         
-        ArrayList<Map.Entry<Integer,Integer>> numCountsAL = new ArrayList<>();
-        for (Map.Entry<Integer,Integer> curEntry : numCountsMap.entrySet()) {
-            numCountsAL.add(curEntry);
-        }
-        
-        // sort the ArrayList of HashMap Entries by the descending order of the Value in each Entry
-        // 注意构造 new Comparator 的语法！！！
-        Collections.sort(numCountsAL, new Comparator<Map.Entry<Integer,Integer>>() {
-            @Override
-            public int compare(Map.Entry<Integer,Integer> entry1, Map.Entry<Integer,Integer> entry2) {
-                int value1 = entry1.getValue();
-                int value2 = entry2.getValue();
-                // 要求降序排列 ！
-                if (value1 > value2)
-                    return -1;
-                else if (value1 < value2)
-                    return 1;
-                else // ==
-                    return 0;
+        public List<Integer> topKFrequent(int[] nums, int k) {
+            
+            // <number, frequency>
+            Map<Integer, Integer> map = new HashMap<>();
+            for(int n: nums){
+                map.put(n, map.getOrDefault(n, 0) + 1);
             }
-        });
-        
-        ArrayList<Integer> result = new ArrayList<>();
-        for (int i = 0; i < Math.min(k, numCountsAL.size()); i++)
-            result.add(numCountsAL.get(i).getKey());
-        return result;
+
+            // <frequency , list of numbers that appeared at this frequency>
+            TreeMap<Integer, List<Integer>> freqMap = new TreeMap<>();
+            for(int num : map.keySet()){
+               int freq = map.get(num);
+               if(!freqMap.containsKey(freq)){
+                   freqMap.put(freq, new LinkedList<>());
+               }
+               freqMap.get(freq).add(num);
+            }
+
+            List<Integer> res = new ArrayList<>();
+            while(res.size()<k){
+                Map.Entry<Integer, List<Integer>> entry = freqMap.pollLastEntry();
+                res.addAll(entry.getValue());
+            }
+            return res;
+        }
     }
     
     
-    // 方法3：我的另一种朴素方法，比上一个朴素方法快一些。
+    // 方法2：我的朴素方法，有点 Bucket Sort 的味道
     // 做一个长度为n+1的数组。如果一个数x出现的次数为m，就在这个数组的第m个index上存下x
     // 但还有可能另一个数y也出现了m次，也需要记在index = m的位置，所以这个数组，其中的每个元素都得是一个ArrayList
     // 记录完以后，从这个数组的尾部开始往前捋。捋满了k个数，或者捋到了数组的头部以后，即得到答案
