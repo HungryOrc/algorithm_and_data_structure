f8a94f08eabf79d249cd0440a0317e2d270cd095
HungryOrc <shu.da.the119@gmail.com>
2017-05-08 21:45:35 -0400

    Update SortCharactersByFrequency.java

--- SortCharactersByFrequency.java
+++ SortCharactersByFrequency.java
@@ -20,7 +20,39 @@ Note that 'A' and 'a' are treated as two different characters. */
 
 public class Solution 
 {
-    // 我的一种朴素做法。速度还可以
+    // 方法1：用 Priority Queue 做
+    public class Solution {
+        public String frequencySort(String s) {
+            Map<Character, Integer> map = new HashMap<>();
+            for (char c : s.toCharArray()) {
+                if (map.containsKey(c)) {
+                    map.put(c, map.get(c) + 1);
+                } else {
+                    map.put(c, 1);
+                }
+            }
+            PriorityQueue<Map.Entry<Character, Integer>> pq = new PriorityQueue<>(
+                new Comparator<Map.Entry<Character, Integer>>() {
+                    @Override
+                    public int compare(Map.Entry<Character, Integer> a, Map.Entry<Character, Integer> b) {
+                        return b.getValue() - a.getValue();
+                    }
+                }
+            );
+            pq.addAll(map.entrySet());
+            StringBuilder sb = new StringBuilder();
+            while (!pq.isEmpty()) {
+                Map.Entry e = pq.poll();
+                for (int i = 0; i < (int)e.getValue(); i++) {
+                    sb.append(e.getKey());
+                }
+            }
+            return sb.toString();
+        }
+    }
+    
+    
+    // 方法2：我的一种朴素做法。速度还可以
     // 注意1：HashMap 的 EntrySet
     // 注意2：sort ArrayList，把里面的 Comparator 里面的 compare 方法做一个 Override
     public String frequencySort(String s) 
