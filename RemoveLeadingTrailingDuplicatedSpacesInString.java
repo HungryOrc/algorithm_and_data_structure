3ad69c00d976c1dec2167d54db52cdccd5bbf2ea
HungryOrc <shu.da.the119@gmail.com>
2017-05-14 22:35:43 -0400

    Update RemoveLeadingTrailingDuplicatedSpacesInString.java

--- RemoveLeadingTrailingDuplicatedSpacesInString.java
+++ RemoveLeadingTrailingDuplicatedSpacesInString.java
@@ -37,5 +37,44 @@ public class Solution {
 
 
 // 方法2: 两个挡板，一快一慢。Laioffer 的方法
+public class Solution {
+  
+  public String removeSpaces(String input) {
+    if (input == null || input.length() == 0) {
+      return "";
+    }
+    
+    int slow = 0;
+    int fast = 0;
+    int wordCount = 0;
+    char[] cArray = input.toCharArray();
+    
+    while (true) {
+      // 消灭所有的空格
+      while (fast < cArray.length && cArray[fast] == ' ') {
+        fast ++;
+      }
+      // 注意！可能此时消灭的是最后的一个或几个空格！那么现在就应该判断是否到了尾部！！！
+      // 如果现在不判断，后面才判断，是会出错的！！！
+      if (fast == cArray.length) {
+        break;
+      }
+      // 如果不是第一个单词，则要在这单词的前面加一个空格
+      if (wordCount > 0) {
+        cArray[slow] = ' ';
+        slow ++;
+      }
+      // 如果不是空格，则加到结果里去
+      while (fast < cArray.length && cArray[fast] != ' ') {
+        cArray[slow] = cArray[fast];
+        slow ++;
+        fast ++;
+      }
+      // 单词个数+1
+      wordCount ++;
+    }
+    
+    return new String(cArray, 0, slow);
+  }
 
-
+}
