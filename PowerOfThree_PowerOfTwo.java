08b0db67835e67903f3ffa62bbb5e145f558a8c7
HungryOrc <shu.da.the119@gmail.com>
2017-05-21 22:01:18 -0400

    Update and rename PowerOfThree_And_PowerOfTwo.java to PowerOfThree_PowerOfTwo.java

--- PowerOfThree_PowerOfTwo.java
+++ PowerOfThree_PowerOfTwo.java
@@ -1,25 +1,23 @@
+// Power of 3 和 Power of 2 是一样的 ！！！
+
 /* Given an integer, write a function to determine if it is a power of three.
 Follow up: Could you do it without using any loop / recursion?   */
 
 public class Solution {
     
-    // 自然想法：不断地除以 3，最后得 1 则为true，否则为false
+    // 方法1：用 Recursion。不要一个一个3地除下去！！ 那样太慢 ！！
     public boolean isPowerOfThree(int n) {
-        
-        if (n <= 0)
-            return false;
-        
-        while (n % 3 == 0)
-            n = n / 3;
-            
-        if (n != 1)
-            return false;
-        else
+        if (n == 3 || n == 1) {
             return true;
+        } else if (n == 0) {
+            return false;
+        }
+        
+        return (n % 3 == 0) && (isPowerOfThree(n / 3));
     }
     
     
-    // Ref: https://leetcode.com/articles/power-of-three/
+    // 方法2，Ref: https://leetcode.com/articles/power-of-three/
     /* let n = 3^i, then we have:
      i = log_3(n) => i = log_10(n) / log_10(3)
      n is a power of 3 if and only if i is an integer. 
@@ -31,5 +29,4 @@ public class Solution {
        return (i % 1 == 0);
     }
      
-    
 }
