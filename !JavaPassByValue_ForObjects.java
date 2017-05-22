757ea3da77e54cbe6156c964ce5acb5342b345a1
HungryOrc <shu.da.the119@gmail.com>
2017-05-22 14:14:40 -0400

    Update !JavaPassByValue_ForObjects.java

--- !JavaPassByValue_ForObjects.java
+++ !JavaPassByValue_ForObjects.java
@@ -1,6 +1,6 @@
 /* Java ALWAYS pass by value, NEVER pass by reference. 
 This is also true for Objects. When passing an Object to a method as its parameter, 
-Java is actually passing the <strong>value of the address</strong> of the Object, <strong>not the reference</strong> of the Object. */
+Java is actually passing the 地址的值 of the Object, 而非reference of the Object. */
 
 public static void main(String[] args) {   	
   Object x = new ...
