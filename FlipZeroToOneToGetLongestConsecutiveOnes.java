4452b7222be91aa64758a164ce9f071355468b04
HungryOrc <shu.da.the119@gmail.com>
2017-05-22 22:37:18 -0400

    Create FlipZeroToOneToGetLongestConsecutiveOnes.java

---
+++ FlipZeroToOneToGetLongestConsecutiveOnes.java
@@ -0,0 +1,13 @@
+/* Given a 0-1 array, you can flip at most k zeroes to ones. Find the longest subarray that consists of all ones.
+
+思路：sliding window，快慢指针
+找到最多含有 k 个 0 的 最长的 window，除了这个 k 个 0 以外，都是 1
+注意 ！ 是最多含有 k 个 0 ！ 不是一定含有 k 个 0 ！ */
+
+public class Solution {
+
+    public int flipZeroToOne(int[] input) {
+    
+    
+    }
+}
