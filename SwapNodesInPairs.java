b3d87398875e6788dc9a4f31cf95b3854b676970
HungryOrc <shu.da.the119@gmail.com>
2017-05-24 21:30:18 -0400

    Update SwapNodesInPairs.java

--- SwapNodesInPairs.java
+++ SwapNodesInPairs.java
@@ -31,39 +31,9 @@ public class Solution {
    }
     
     
-    // 方法2：Iteration
-    // 注意这里是一种以 “一对nodes” 为一个考量单位的 iteration，和以往的 iteration 不太一样。详见下面的注释
+    // 方法2：Iteration。Laioffer
     public ListNode reverseInPairs(ListNode head) {
+        ListNode dummy = new ListNode(0);
         
-        if (head == null || head.next == null)
-            return head;
-            
-        ListNode originalNext = head.next;
-        ListNode prevNode = new ListNode(0);
-        
-        while (head.next != null && head.next.next != null)
-        {
-            ListNode temp = head.next;
-            head.next = temp.next;
-            temp.next = head;
-            
-            head = head.next;
-            
-            prevNode.next = temp;
-            prevNode = temp.next;
-        }
-        // 一对一对的结构不再完整以后，即List全部结束了，或者还剩一个单的node
-        if (head.next == null) // List全部结束了
-        {
-            // do nothing
-        }
-        else if (head.next != null && head.next.next == null) // 还剩一个单的node
-        {
-            prevNode.next = head.next;
-            head.next.next = head;
-            head.next = null;
-        }
-        return originalNext;
-    }
-    
+    }   
 }
