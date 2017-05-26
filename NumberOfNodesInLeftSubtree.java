f2fc81ca1875400f5bfe07cff85e11925a107e58
HungryOrc <shu.da.the119@gmail.com>
2017-05-26 14:33:52 -0400

    Create NumberOfNodesInLeftSubtree.java

---
+++ NumberOfNodesInLeftSubtree.java
@@ -0,0 +1,14 @@
+// 找到每个node的左子树里的总nodes个数，并存在本node的相应field里
+
+class TreeNode {
+    TreeNode left, right;
+    int value;
+    int numOfNodesInLeftSubtree;
+}
+
+public class Solution {
+
+
+
+
+}
