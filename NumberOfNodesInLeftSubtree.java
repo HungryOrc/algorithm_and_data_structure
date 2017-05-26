4b67ca99da6236abc1a66110d991de9e84faf1b6
HungryOrc <shu.da.the119@gmail.com>
2017-05-26 14:39:28 -0400

    Update NumberOfNodesInLeftSubtree.java

--- NumberOfNodesInLeftSubtree.java
+++ NumberOfNodesInLeftSubtree.java
@@ -8,7 +8,21 @@ class TreeNode {
 
 public class Solution {
 
+    public void findNumberOfNodesInLeftSubtree(TreeNode root) {
+        countNodes(root);
+   
+    }
 
-
-
+    private int countNodes(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        
+        int leftCount = countNodes(node.left);
+        int rightCount = countNodes(node.right);
+        
+        node.numOfNodesInLeftSubtree = leftCount;
+        
+        return leftCount + rightCount + 1; // 向本node的父节点返回的信息 ！！！
+    }
 }
