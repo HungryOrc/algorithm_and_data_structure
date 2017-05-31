f20df3a908429a56e941f296e8815dae639799c1
HungryOrc <shu.da.the119@gmail.com>
2017-05-31 10:38:24 -0400

    Update MaxSumSubarray.java

--- MaxSumSubarray.java
+++ MaxSumSubarray.java
@@ -7,10 +7,34 @@ Given the array [−2,2,−3,4,−1,2,1,−5,3], the contiguous subarray [4,−1
 
 public class Solution {
 
-    // 方法1：Prefix-Sum
+    // 方法1：DP
+    // dp数组里 dp[i] 的意思是，结尾在第i个元素的所有subarray里，sum最大的值
+    public int largestSum(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return Integer.MIN_VALUE;
+        }
+
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        int maxSubarraySum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            if (dp[i - 1] >= 0) {
+                dp[i] = dp[i - 1] + nums[i];
+            } else {
+                dp[i] = nums[i];
+            }
+
+            maxSubarraySum = Math.max(dp[i], maxSubarraySum);
+        }
+        return maxSubarraySum;
+    }
+
+    
+    // 方法2：Prefix-Sum
     public int maxSubArray(int[] nums) {
         if (nums == null || nums.length == 0) {
-            return 0;
+            return Integer.MIN_VALUE;
         }
         
         int maxSum = Integer.MIN_VALUE;
@@ -26,11 +50,11 @@ public class Solution {
     }
 
     
-    // 方法2：Greedy。如果不大于零，就清零！！！
+    // 方法3：Greedy。如果不大于零，就清零！！！ 这个思路其实和DP方法的思路是完全一样的 ！
     // Ref: http://www.jiuzhang.com/solutions/maximum-subarray/
     public int maxSubArray(int[] A) {
         if (A == null || A.length == 0){
-            return 0;
+            return Integer.MIN_VALUE;
         }
         
         int max = Integer.MIN_VALUE, sum = 0;
