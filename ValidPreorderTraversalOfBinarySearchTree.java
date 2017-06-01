408fead9f3c34d92542c1e3eb6678e372d9be677
HungryOrc <shu.da.the119@gmail.com>
2017-06-01 11:16:38 -0400

    Update ValidPreorderTraversalOfBinarySearchTree.java

--- ValidPreorderTraversalOfBinarySearchTree.java
+++ ValidPreorderTraversalOfBinarySearchTree.java
@@ -8,7 +8,71 @@ Example:
 Follow up: Could you do it using only constant space complexity?   */
 
 
-/* 方法1：我的方法，Time: O(n)
+/* 方法1：用 Stack 的 Iterative 方法
+Time: O(n), Space: O(n)
+Ref: https://discuss.leetcode.com/topic/21217/java-o-n-and-o-1-extra-space
+
+Preorder，数组左边第一个元素一定是整个树的root。先把它push进stack里。
+然后从第二个元素开始，从左往右看各个元素，
+如果比stack顶部的元素小，那么就push到stack里去，这意味着这些新到的元素，都是处于越来越深的左子方向上。
+如果比stack顶部的元素大，那么就要从stack里pop元素出来。这意味着新到的元素是stack里的元素的右子，而且
+还未必是栈顶元素的右子，有可能是栈顶元素的爸爸，或者栈顶元素的爷爷的右子...... 所以要不断地pop，直到栈顶元素大于新到的元素，或者栈pop空了，
+这时再把新到的元素push到栈里去。
+
+在这个过程中，什么情况下是违反了BST的规则呢 ？？？！！！
+这个过程里，我们是先从上往下地逐层填满各个左子，再从下往上地逐层填满各个右子，
+如果填右子的过程里，又来了一个过于小的元素，是必须填到左子去的，那就是违规了 ！！！
+所以我们必须不断更新目前处理到的nodes的value的下限是多少。这个下限就在从stack里pop元素出来的时候更新 ！！！   */
+
+public class Solution {
+    
+    public boolean verifyPreorder(int[] preorder) {
+        if (preorder == null || preorder.length <= 1) {
+            return true;
+        }
+        
+        Deque<Integer> valueStack = new LinkedList<>();
+        valueStack.push(preorder[0]);
+        
+        int curLowerBound = Integer.MIN_VALUE;
+        
+        for (int i = 1; i < preorder.length; i++) {
+            int curValue = preorder[i];
+            
+            if (curValue <= curLowerBound) {
+                return false;
+            }
+            
+            if (curValue < valueStack.peek()) {
+                valueStack.push(curValue);
+            } else {
+                while (!valueStack.isEmpty() && curValue > valueStack.peek()) {
+                    curLowerBound = Math.max(curLowerBound, valueStack.pop());
+                }
+                valueStack.push(curValue);
+            }
+        }
+        return true;
+    }
+}
+
+// 上面的方法的优化：空间从 O(n) -> O(1)
+// Ref: https://discuss.leetcode.com/topic/21217/java-o-n-and-o-1-extra-space
+// Use the give array as the stack:
+public boolean verifyPreorder(int[] preorder) {
+    int low = Integer.MIN_VALUE, i = -1;
+    for (int p : preorder) {
+        if (p < low)
+            return false;
+        while (i >= 0 && p > preorder[i])
+            low = preorder[i--];
+        preorder[++i] = p;
+    }
+    return true;
+}
+
+
+/* 方法2：我的 Recursion 方法，Time: O(n)
 思路的缘由，参考我总结的另一题：Reconstruct BST with Preorder Traversal
 
 一个合格的关于BST的 Preorder Traversal（这里是作为一个数组），一定是开头即整个树的 root 的value。
@@ -52,4 +116,3 @@ public class Solution {
         verifyPreorder(preorder, curNodeValue, upperBound);
     }   
 }
-
