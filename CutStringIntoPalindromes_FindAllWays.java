d072c0f01413b75961db4c5ae6700c7c20f309d0
HungryOrc <shu.da.the119@gmail.com>
2017-06-08 00:34:13 -0400

    Create CutStringIntoPalindromes_FindAllWays.java

--- CutStringIntoPalindromes_FindAllWays.java
+++ CutStringIntoPalindromes_FindAllWays.java
@@ -8,50 +8,41 @@ Given s = "aab", return:
   ["a","a","b"]
 ] */
 
+// DFS
 public class Solution {
     
-    // 我自己的方法：DFS
     public List<List<String>> partition(String s) {
-        
         List<List<String>> result = new ArrayList<>();
         if (s == null || s.length() == 0) {
             return result;
         }        
         
-        dfs(s, 0, new ArrayList<String>(), result);
+        dfs(s, s.toCharArray(), 0, new ArrayList<String>(), result);
         return result;
     }
     
-    private void dfs(String s, int startIndex, ArrayList<String> permutation,
-        List<List<String>> result) {
-    
-        if (startIndex == s.length()) {
+    private void dfs(String s, char[] cArray, int start, ArrayList<String> permutation, List<List<String>> result) {
+        if (start == cArray.length) {
             result.add(new ArrayList<String>(permutation));
             return;
         }
-        
-        // 因为在Strin.substring函数里，startIndex是inclusive的，endIndex是exclusive的，
-        // 所以 endIndex 要起始于 startIndex + 1，终止于 = length 而非 < length！！
-        for (int endIndex = startIndex + 1; endIndex <= s.length(); endIndex++) {
-            
-            String subString = s.substring(startIndex, endIndex);
-            
-            if (isPalindrome(subString)) {
-                permutation.add(subString);
+
+        for (int end = start; end < cArray.length; end ++) {
+            if (isPalindrome(cArray, start, end)) {
+              
+                permutation.add(s.subString(start, end + 1));
+                
                 // 注意！下一次的分析的开始位置，是这一次的subString的结束位置+1，
-                // 也就是当前的endIndex位置！！
-                dfs(s, endIndex, permutation, result);
+                dfs(s, cArray, end + 1, permutation, result);
+                
                 permutation.remove(permutation.size() - 1);
             }
         }
     }
     
-    private boolean isPalindrome(String string) {
-        char[] cArray = string.toCharArray();
-        int len = cArray.length;
-        
-        for (int i = 0; i < len / 2; i++) {
-            if (cArray[i] != cArray[len - i - 1]) {
+    private boolean isPalindrome(char[] cArray, int i, int j) {
+        while(i < j) {
+            if (cArray[i] != cArray[j]) {
                 return false;
             }
         }
