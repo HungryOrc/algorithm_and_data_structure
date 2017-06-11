019081470f92f7814ddfc492cfc804eabcf39f62
HungryOrc <shu.da.the119@gmail.com>
2017-06-11 12:03:58 -0400

    Create LargestSquareOfMatchSticks.java

---
+++ LargestSquareOfMatchSticks.java
@@ -0,0 +1,14 @@
+/* Given a bunch of matches (火柴), find the largest (area) squre formed by these matches, if any.
+  __ __ __ __ __ __ __
+ |        |     |__
+ |__ __ __|     |__|
+ |  |
+ |  |__ __ __    __
+ |__|           |  |
+    |
+ 
+ 思路：参考我总结的另一题：Largest Square Surrounded by Ones
+ 
+ 
+ 
+ */
