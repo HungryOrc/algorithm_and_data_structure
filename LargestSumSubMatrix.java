03bfeefdb8d6a0a4ab473e5ca251128482b0526e
HungryOrc <shu.da.the119@gmail.com>
2017-06-12 23:41:24 -0400

    Create LargestSumSubMatrix.java

--- LargestSumSubMatrix.java
+++ LargestSumSubMatrix.java
@@ -28,11 +28,27 @@ the largest submatrix sum is (-1) + 4 + 1 + 1 + (-1) + 1 + 1 + 1 = 7.
 按之前的讨论，一共有 O(n^4) 种sub matrix，所以总的计算时间是 O(n^5)。   */
 
 
-/* 方法3：
+/* 方法3：很巧妙，再进一步，用 2个 DP Matrix 或者说 Prefix Sum Matrix 来做 ！！！
+利用方法2里的 Prefix Sum of Each Row，我们把这个helper matrix的每一列，都再做一个逐元素的 prefix sum，这样又诞生一个 prefix sum matrix，
+这个新的helper matrix，就是 Prefix Sum of Each (Sub) Square ！！！ 我们把它简称为 matrix DP2，
+（把 Prefix of Each Row 简称为 matrix DP1）
+可以想到，它的每一个元素(i, j)，就是最初的matrix里从左上角的cell(0, 0) 到任何一个cell(i, j) ——— 作为右下角，
+所划定的sub square里的所有元素之和 ！！！
+
+那么，我们要计算 左上角为(p, q)，右下角为(i, j) 的sub square 里的所有元素之和，按照小学奥数几何，就等于
+Sum((p,q),(i,j)) = DP2(i, j) - DP2(p-1, j) - DP2(q-1, i) + DP2(p-1, q-1)，这个步骤用O(1)的时间就算好了！
+而一共有 O(n^4) 种sub square，所以一共需要时间是 O(n^4)。   */
 
 
-
-*/
+/* 方法4：终极改进！！！ 时间O(n^3) ！！！ 非常巧妙
+首先还是需要方法3里的两个DP helper matrixes。方法3里确定一个square，是要确定4个坐标，左上角的2个坐标和右下角的2个坐标，所以共需 O(n^4)。
+我们进一步改进。首先确定square的上下row（both inclusive），比如说 row i 和 row j。相当于两条水平线夹着一个区域。
+然后因为在方法3里已经有了 Prefix Sum of Each Square，所以在上述的区域里，对于每一列s，我们可用 O(1) 的时间求出
+row i，row j，col 0 和 col s 所包围的square的sum，为：DP2[j][s] - DP2[i][s] + DP1[i][s]。
+那么我们要求的拥有max sum的square，如果是被 row i，row j，col s 和 col t 包围的话，在 i 和 j 一定的条件下，
+我们可以用 O(n) 的时间同时求出 t 和 s ！！！
+方法就是一维数组的 subarray with max sum ！！！ 相当于把row i 到 row j 压扁到一维的一条线 ！！！
+然后用DP也好可以说是Prefix Sum of Subarray也好，在one pass的过程里求出max sum subarray的start index 和 end index，即 t 和 s    */
 
 public class Solution {
   
