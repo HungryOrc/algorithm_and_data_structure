148a747b71bd5ff833216e30b55a37017db3dfc1
HungryOrc <shu.da.the119@gmail.com>
2017-06-16 19:56:15 -0400

    Update RemoveDuplicatesInUnsortedArray_Repeatedly.java

--- RemoveDuplicatesInUnsortedArray_Repeatedly.java
+++ RemoveDuplicatesInUnsortedArray_Repeatedly.java
@@ -10,38 +10,62 @@ Examples:
 (2) "aabccdc" → "bccdc" → "bdc" */
 
 
-// 方法1：我自己的方法，用一个模拟的栈（用数组的左半段模拟一个栈的行为），再加上两个指针一快一慢。真正上场的时候用这个方法。
-// 这个方法是源于 Laioffer 的方法
-public class Solution {
-  public String deDup(String input) {
-    if (input == null || input.length() <= 1) {
-        return input;
+// 方法1：我自己的方法，用一个模拟的栈（用数组的左半段模拟一个栈的行为），再加上两个指针一快一慢，同向而行，
+// 慢指针的左边不含慢指针自己，是栈里的内容，也就是最后要保留的sub array；快指针所指向的是当前正要被处理的元素。
+//
+// 惊人之处在于：
+// 这个代码，与 Remove Duplicates in SORTED Array_REMOVE ALL 那一题的代码，是完全一致的 ！！！
+//
+// 注意体会它们之所以一致的原因：
+// 本题是 unsorted array 然后要反复删除所有重复内容，包括一开始就相邻的重复内容，和一开始不相邻但是随着处理过程而被相邻的重复内容；
+//     比如 {1, 4, 4, 4, 4, 1, 1} 这样的数组，把中间的4都“去重”以后，
+//     slow指针在第一个4那里，它左边的第一个1是当前要保留的唯一内容；fast指针在第二个1那里，即所有4后面的第一个1；
+//     我们比较fast和slow-1的内容，发现它们是相同的，所以fast及其之后的所有1，以及slow-1处的1，都要被去重的。
+// 那一题是 sorted array 然后要删除所有相邻的重复的内容，因为一开始就排序好的，所以一开始不相邻的元素就不可能相等即不可能重复
+//     比如 {1, 1, 4, 4, 4} 这样的数组，一开始slow和fast都在index 1那里，即都在第二个1那里，
+//     slow-1和fast的元素是相等的，所以fast先要移动到第一个4那里去，以摆脱所有的1，
+//     然后slow--，即slow=0，把所有元素都退空
+// 这样可以看出，这两题内在的逻辑是一致的 ！！！
+
+public int[] dedup(int[] array) {
+    if (array == null || array.length <= 1) {
+      return array;
     }
-  
-    char[] cArray = input.toCharArray();
-    int slow = -1; // 当前的“栈”顶
-    int fast = 0; // 当前在char数组里访问到第几个char
     
-    while (fast < cArray.length) {
-      // slow==-1 是当前stack空了的情况
-      if (slow == -1 || cArray[fast] != cArray[slow]) { 
-        slow ++;
-        cArray[slow] = cArray[fast];
-        fast ++;
-      }
-      // 当前栈不空，而且当前在char array里正在被访问的char 等于 “栈”顶的char
-      else { 
-        char duplicatedChar = cArray[slow];
-        slow --; // 把当前栈顶的char弹出去不要了。注意！只用弹一次！因为再往下的一个一定不同了
-        // 把数组里从现在位置开始往右的所有等于栈顶char的char都略过
-        while (fast < cArray.length && cArray[fast] == duplicatedChar) { 
+    // array[slow - 1] is the latest sample we encountered that might be
+    // duplicated or not
+    int slow = 1, fast = 1;
+    
+    while (fast < array.length) {
+    
+      // 这一题的解法的关键在于，把 “当前重复” 和 “当前没重复” 这两种情况，分别放在两个 “处理包” 里 ！！！
+      
+      // 1. 发现当前重复 的处理包：
+      if (array[fast] == array[slow - 1]) {
+        while (fast < array.length && array[fast] == array[slow -1]) {
           fast ++;
         }
+        // 这句话放在“处理包”里做就比较好，是一个整体的一部分
+        // 不“打包”的话，散在外面，就不好处理slow的退一步和之前的while loop里fast的多次进步的  配  套  关  系 ！！！
+        slow --; 
+      }
+      
+      // 2. 当前没重复 的处理包：
+      if (fast < array.length && // 别忘了还是得先看一下fast出了上界没有。没出的话才有以下的处理的必要
+          (slow == 0 || // 注意这种可能性 ！ slow 可能已经退到数组的第一位了，slow - 1 就出下界了 ！！！
+           array[fast] != array[slow -1])) {
+        
+        array[slow] = array[fast];
+        slow ++;
+        fast ++;
       }
     }
-    // slow指向的是当前栈顶我们要保留的char，所以要再往右边一格，因为上限是exclusive的
-    return new String(cArray, 0, slow + 1); 
-  }
+    
+    // Arrays.copyOf 函数里的第二个参数(int)的意义是substring的长度（从index 0开始计算），
+    // 而非substring的结束点的index或者结束点的后一位的index
+    return Arrays.copyOf(array, slow); // index slow is excluded, so the length shall be equals "slow"
+    // 注意：到了这里，slow可能==0，即例如 input array = {4, 4, 4, 4} 的情况
+  } 
 }
 
 
