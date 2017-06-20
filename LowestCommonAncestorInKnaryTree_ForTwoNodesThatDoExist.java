ab9e710dcf0634df73a30d9ed9c37f2484e4b0b7
HungryOrc <shu.da.the119@gmail.com>
2017-06-20 17:43:49 -0400

    Update LowestCommonAncestorInKnaryTree_ForTwoNodesThatDoExist.java

--- LowestCommonAncestorInKnaryTree_ForTwoNodesThatDoExist.java
+++ LowestCommonAncestorInKnaryTree_ForTwoNodesThatDoExist.java
@@ -2,8 +2,30 @@
 
 public class Solution {
 
-    public TreeNode LCA_2Nodes_KBranchTree(TreeNode one, TreeNode two) {
-
+    public TreeNode LCA_2Nodes_KBranchTree(TreeNode root, TreeNode one, TreeNode two) {
+        if (root == null) {
+            return null;
+        }
+        if (root == one || root == two) {
+            return root;
+        }
     
+        int numOfNodesFound = 0;
+        TreeNode tmpOutcome = null;
+        
+        for (TreeNode child : root.children) {
+            TreeNode outcomeFromThisChild = LCA_2Nodes_KBranchTree(child, one, two);
+            
+            if (outcomeFromThisChild != null) {
+                numOfNodesFound ++;
+            
+                if (numOfNodesFound == 2) {
+                    return root;
+                } else { // numOfNodesFound == 1
+                    tmpOutcome = child;
+                } 
+            }
+        }
+        return tmpOutcome;
     }
 }
