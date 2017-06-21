cf7ba3c0d1c4970f467042cc2d228e7bade4d198
HungryOrc <shu.da.the119@gmail.com>
2017-06-21 00:26:25 -0400

    Create UniquePathsToGoAcrossAMatrix.java

---
+++ UniquePathsToGoAcrossAMatrix.java
@@ -0,0 +1,26 @@
+/* A robot is located at the top-left corner of a m x n grid (where m > 0 and n > 0).
+The robot can only move either down or right for 1 slot in each step. 
+The robot is trying to reach the bottom-right corner of the grid.
+How many possible unique paths are there?   */
+
+// 方法：DP，我自己的做法
+public class Solution {
+  
+  public int uniquePaths(int m, int n) {
+    int[][] dp = new int[m][n];
+    
+    for (int i = 0; i < m; i++) {
+      dp[i][0] = 1;
+    }
+    for (int j = 0; j < n; j++) {
+      dp[0][j] = 1;
+    }
+    
+    for (int i = 1; i < m; i++) {
+      for (int j = 1; j < n; j++) {
+        dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+      }
+    }
+    return dp[m - 1][n - 1];
+  }
+}
