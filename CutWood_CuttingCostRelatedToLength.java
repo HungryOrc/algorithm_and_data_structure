b2b4f39ad79815eba9f927fa7efdb7b743802f89
HungryOrc <shu.da.the119@gmail.com>
2017-06-23 21:22:25 -0400

    Update CutWood_CuttingCostRelatedToLength.java

--- CutWood_CuttingCostRelatedToLength.java
+++ CutWood_CuttingCostRelatedToLength.java
@@ -1,4 +1,6 @@
-/* There is a wooden stick with length L >= 1, we need to cut it into pieces, where the cutting positions are defined in an int array A. 
+/* 这是一种传说中的二维DP题
+
+There is a wooden stick with length L >= 1, we need to cut it into pieces, where the cutting positions are defined in an int array A. 
 The positions are guaranteed to be in ascending order in the range of [1, L - 1]. 
 The cost of each cut is the length of the stick segment being cut. 
 Determine the minimum total cost to cut the stick into the defined pieces.
