5ec854bd5c3a2d426b1b03024169dea5b3833b72
HungryOrc <shu.da.the119@gmail.com>
2017-06-27 21:57:00 -0400

    Update ReconstructBinaryTreeWithLevelOrderAndInorder.java

--- ReconstructBinaryTreeWithLevelOrderAndInorder.java
+++ ReconstructBinaryTreeWithLevelOrderAndInorder.java
@@ -23,15 +23,19 @@ Than we can get the corresponding binary tree:
  如果比root的index大，就把本数放到另一个list里去，作为将来构建当前root的右子树的元素。
  （注意，由于 level order的性质，比root大和比如root小的数，会在level order数组里交替出现！！）
  
- 举例：比如上面题目里给的那个 binary tree，它的root是5，也是postorder的最后一位：
- postorder: 1 4 3 11 8 <5>
- inorder:   1 3 4 <5> 8 11
+ 举例：比如上面题目里给的那个 binary tree，它的root是5，也是level order的第一位：
+ levelorder: <5> 3 8 1 4 11
+ inorder:    1 3 4 <5> 8 11
  然后，在inorder里面找到5，那么在inorder里，5之前的，就是5的左子树所有的nodes；5之后的，就是5的右子树所有的nodes。
- 然后，就要按照这个，把postorder里的数字也都分为对应的两部分：
- postorder: 1 4 3    11 8    <5>
- inorder:   1 3 4    <5>    8 11
- 然后就对inorder里的左半部分和postorder里相应的部分进行关于左子树的recursion处理；
- 对inorder里的右半部分和postorder里相应的部分进行关于右子树的recursion处理。这些处理和之前对于整个树的处理是同理的    */
+ 然后，就要按照这个，把level order里的数字也都分为对应的两部分：
+ levelorder: <5>   3 1 4   8 11
+ inorder:    1 3 4   <5>   8 11
+ 但是上面这一步，由于level order里比root大和小的数是交替出现的，所以不能再沿用level order数组的一部分来做，不得已只能
+ 重新构建2个list，一个一个往这两个list里加数！！！
+ 这么加的话，在每个list里，每个数的前后相对关系，是保持了与level order数组里的相对关系相一致的 ！！！
+ 
+ 然后就对inorder里的左半部分和level order里相应的部分进行关于左子树的recursion处理；
+ 对inorder里的右半部分和level order里相应的部分进行关于右子树的recursion处理。这些处理和之前对于整个树的处理是同理的    */
 
 /* public class TreeNode {
  *   public int key;
