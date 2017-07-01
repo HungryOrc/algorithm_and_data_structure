47facbea04d45887893aabb51945ae7f6b6234db
HungryOrc <shu.da.the119@gmail.com>
2017-07-01 19:57:30 -0400

    Update MaxProductOfLengthOfStringsWithoutCommonChars.java

--- MaxProductOfLengthOfStringsWithoutCommonChars.java
+++ MaxProductOfLengthOfStringsWithoutCommonChars.java
@@ -9,6 +9,40 @@ Examples:
 dictionary = [“abcde”, “abcd”, “ade”, “xy”], the largest product is 5 * 2 = 10 (by choosing “abcde” and “xy”)  */
 
 
+/* 思路：用 max heap 来存不同的string pair的indexes，按照这个pair里的2个string的长度的乘积，来排列这个max heap。
+当然，要这么做，首先题目给的string array必须是按照string的length降序排列的。
+然后，把数组里的第一个和第二个数放到heap里去。因为是放indexes，所以放的是 0 和 1 这两个整数。
+然后，只要heap不空，就从其顶部poll元素出来。每个元素是一个长度为2的整形数组，分别是2个string在初始的string array里的index，设为i和j。
+如果i和j代表的这两个string不share任何common char，则记录下它们的长度的乘积，如果比maxProduct大，就更新maxProduct的值。
+
+然后，要根据它expand两组接下来的pairs。分别是 i+1和j，以及 i和j+1 这两个pair。
+不过，每一个index pair要放到heap里之前，都要满足以下几个条件：
+两个index都没越界、两个index不相同、这个index pair没有被访问过、这个index pair所代表的string pair的长度乘积大于当前的 max product。
+
+为了判断一个index pair是否被访问过，即去重的问题，我们用一个二维n*n的boolean matrix来做。
+不用set来做，是为了避免写坑多的 equals，hashcode 等函数。
+
+最后，这个方法里，为了判断两个string之间是否有common char，是用了 bit mask 来做。这个做法值得长期推广！
+详情见最后一个helper function。
+
+
+这个方法里必须注意的一个重要问题 ！！！
+虽然我们用了max heap，但不能在得到一个不重复char的string pair以后，就直接return 它们的长度的乘积 ！！！
+因为，我们的max heap，并不能保证先填进去的元素，就一定有相对最高的length product！
+而只能保证，先填进去的元素，大概率有相对更高的 length product！
+比如，index 为 1 和 2 的pair，会比 index 为 0 和 5 的pair 更早地被填入heap，但就 length product 来做，很可能后者更大 ！！！
+所以这种方法，一定要做到尽头，做到 heap 空掉为止。不可中间结束。
+
+但是相对于 双层循环 的暴力方法，用heap做的方法还是会快不少！
+因为毕竟是从string length最长的头部开始做的。后面的各个pair，如果本pair的product不及当前的max product，就不会再考虑
+任何比本pair还要更短的pairs了。所以这相当于减去了很多枝条！！！
+
+这种方法的时间复杂度：O(n^2 * (m + 2logn))，其中n是dict里的string的个数，m是每个string的平均长度。
+n^2是 number of pairs of strings。然后，
+一方面，用heap pop元素的时候，heap最大是n^2的尺寸，所以每次pop都是log(n^2) 即 2logn 的时间复杂度；
+另一方面，每次查看2个string有没有 common char，需要时间 O(m)。所以总的来说，是上面的表达式。 */
+
+// 我的方法，代码虽然很长，但是思路其实很朴素，简明
 public class Solution {
   
 	public int largestProduct(String[] dict) {
