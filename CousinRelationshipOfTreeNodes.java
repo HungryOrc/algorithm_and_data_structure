95d9a616cb7338c00a1f3f6475c30bede8899aff
HungryOrc <shu.da.the119@gmail.com>
2017-07-04 14:20:48 -0400

    Update CousinRelationshipOfTreeNodes.java

--- CousinRelationshipOfTreeNodes.java
+++ CousinRelationshipOfTreeNodes.java
@@ -4,7 +4,8 @@
 
 这一题是给root的。另外会给要找的2个nodes。假设：
 1. The tree is not null.
-2. The 2 nodes do exist in the binary tree whose root is the given root.  */
+2. The 2 nodes do exist in the binary tree whose root is the given root。
+3. 这两个nodes不是同一个node。  */
 
 
 // 方法1：Laioffer很巧妙的方法！！！ 较难想到
@@ -14,15 +15,33 @@ public class Solution {
     boolean isCousinOrNot = false;
 
     public boolean isCousin(TreeNode root, TreeNode one, TreeNode two) {
-	isCousin(root, one, two, 1);
+	getLevelWithSpecialTreatment(root, one, two, 1);
 	return isCousinOrNot;
     }
     
-    private int isCousin(TreeNode root, TreeNode one, TreeNode two, int curLevel) {
+    private int getLevelWithSpecialTreatment(TreeNode root, TreeNode one, TreeNode two, int curLevel) {
     	if (root == null) {
 	    return -1;
 	}
 	
+	if (root == a || root == b) {
+	    return curLevel;
+	}
+	    
+	int resultL = getLevelWithSpecialTreatment(root.left, one, two, curLevel + 1);
+	int resultR = getLevelWithSpecialTreatment(root.right, one, two, curLevel + 1);
+	
+	if (resultL == resultR && 
+	    resultL - curLevel >= 2) { // 关键 1 ！！！ 这一句可以判断出，找到Node one和two的level，是不是当前node的孙子或更下层，
+		                       // 如果是，则意味着，one和two不可能共爹 ！！！
+	    isCousinOrNot = true;
+	}
+	
+	// 关键 2 ！！！
+	// 如果 L 和 R 中的任何一个是-1，即意味着找不到，则返回另一个；
+	// 如果 L 和 R 都不是-1，但是一大一小，即意味着one和two都找到了，但在不同层，这样的话，其实返回谁都无所谓了，
+	// 因为这样的话，到了最后汇总答案的时候，L和R也不可能一致，那时的L和R二者必有一个是-1
+	return resultL > resultR ? resultL : resultR;
     }
 
 }
