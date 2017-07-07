e34c2a417bf8afaccfdd4359b28afa9298c5fa41
HungryOrc <shu.da.the119@gmail.com>
2017-07-06 23:40:29 -0400

    Update MajorityElement_OneHalf.java

--- MajorityElement_OneHalf.java
+++ MajorityElement_OneHalf.java
@@ -6,6 +6,18 @@ public class Solution {
     // 方法1：极为巧妙的方法：Moore voting algorithm
     // 数学证明见：http://www.cs.utexas.edu/~moore/best-ideas/mjrty/
     // Time: O(n), Space: O(1)
+    
+    /* 思路：用别的数 “对消” 目前计数最 “冒尖” 的数 ！！！ 所谓 “枪打出头鸟”：
+       先把数组里的第一个数作为candidate，其计数count初始记为 1. 然后，从第二个数开始，
+       如果它等于当前的candidate，就count++；
+       如果它不等于当前的candidate，
+           如果count当前已经是0了，那么就改换门庭！将candidate改设为当前的数，count重新记为 1；
+           如果count当前 > 0，则 count--   
+        
+       总之，可以这么理解：
+       每一次累加，都是对于当前candidate的增强，
+       而每一次削减，都是  2个不同的数  同时削减 ！！！ 最后的赢家 理  应  能 Survive 这种削减　！！！　*/
+    
     public int majorityElement(int[] nums) {
 
         int candidate = array[0];
@@ -24,14 +36,14 @@ public class Solution {
           }
         }
 
-        // 以下部分是用来检查，我们选出的candidate，到底它的出现次数是否大于等于 n/2
+        // 以下部分是用来检查，我们选出的candidate，到底它的出现次数是否大于 n/2
         count = 0;
         for (int num : array) {
           if (num == candidate) {
             count ++;
           }
         }
-        if (count >= array.length/2) {
+        if (count > array.length/2) {
           return candidate;
         } 
 
