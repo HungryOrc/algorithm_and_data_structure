72fb4d45654dda6581ce0dd5840cf7aee21428de
HungryOrc <shu.da.the119@gmail.com>
2017-07-08 13:29:48 -0400

    Update LongestCommonSubstring.java

--- LongestCommonSubstring.java
+++ LongestCommonSubstring.java
@@ -8,6 +8,19 @@ S = “abcde”, T = “cdf”, the longest common substring of S and T is “cd
 S = "aaaaa", T = "bbb", return "".  */
 
 
+/* 思路：设置一个二维的 DP 数组，dp[i][j] 的意思是，
+在 string S 里，所有以 index = i 处的char结尾的substring，和 string T 里，所有以 index = j 处的char结尾的substring，
+这两类substring之间存在的最长的 common substring 的长度。
+注意 ！！！ 这两类substring的起始位置随意，但结束位置分别必须是 S里的i处 和 T里的j处 ！！！
+
+那么，对于dp[i][j]，i = 0 或 j = 0 的情况下，如果 S[i] == T[j]，则 dp[i][j] = 1，否则为 0.
+上面即第一行和第一列的情况。对于矩阵更内部的情况：
+
+dp[i][j] = 0,                       if S[i] != T[j]
+         = dp[i - 1][j - 1] + 1,    if S[i] == T[j]
+
+最后取整个dp矩阵里的最大值为答案 max length。注意！不是取 dp[n - 1][m - 1]！    */
+
 public class Solution {
   
   public String longestCommon(String s, String t) {
