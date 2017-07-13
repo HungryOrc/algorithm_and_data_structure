e5680f92f11392a5ed7b5572a7ef524a4d2e3884
HungryOrc <shu.da.the119@gmail.com>
2017-07-12 23:27:02 -0400

    Update LongestPalindromeSubstring.java

--- LongestPalindromeSubstring.java
+++ LongestPalindromeSubstring.java
@@ -68,3 +68,64 @@ public class Solution {
 // 每次往两边扩，左-1，右+1，看还是不是palindrome。如果不是，这个中心点就不必再继续下去了。看下一个中心点
 // 这样做法看起来暴力，但时间也是 O(n^2)，和DP方法一样。其实没有任何重复查找，所以不必记忆化任何中间信息 ！！！
 
+// 这个方法的代码看起来有点长，其实思路非常简单明白
+public class Solution {
+  
+  public String longestPalindrome(String s) {
+    if (s == null || s.length() == 0) {
+      return "";
+    } else if (s.length() == 1) {
+      return s;
+    }
+    
+    int n = s.length();
+    int maxLen = 1;
+    int start = 0, end = 0;
+    
+    // palindrome的中心是一个char
+    for (int center = 1; center < n - 1; center++) {
+      for (int radius = 1; radius <= center && radius < n - center; radius ++) {
+        int i = center - radius;
+        int j = center + radius;
+        
+        if (s.charAt(i) != s.charAt(j)) {
+          break;
+        } else {
+          if (j - i + 1 > maxLen) {
+            maxLen = j - i + 1;
+            start = i;
+            end = j;
+          }
+        }
+      }
+    }
+    
+    // palindrome的中心是2个相同的相邻的chars
+    for (int centerL = 1; centerL < n - 1; centerL++) {
+      if (s.charAt(centerL) != s.charAt(centerL + 1)) {
+        continue;
+      } else if (maxLen < 2) {
+        maxLen = 2;
+        start = centerL;
+        end = centerL + 1;
+      }
+      
+      for (int radius = 1; radius <= centerL && radius < n - centerL - 1; radius ++) {
+        int i = centerL - radius;
+        int j = centerL + 1 + radius;
+        
+        if (s.charAt(i) != s.charAt(j)) {
+          break;
+        } else {
+          if (j - i + 1 > maxLen) {
+            maxLen = j - i + 1;
+            start = i;
+            end = j;
+          }
+        }
+      }
+    }
+    
+    return s.substring(start, end + 1);
+  }
+}
