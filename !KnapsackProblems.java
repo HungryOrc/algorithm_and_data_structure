052574835fdbabb8910cdbb6f4808201b74c7cb8
HungryOrc <shu.da.the119@gmail.com>
2017-07-13 18:30:52 -0400

    Update !KnapsackProblems.java

--- !KnapsackProblems.java
+++ !KnapsackProblems.java
@@ -10,3 +10,18 @@ What's the max total weight you can put into the knapsack?
         M[i, w] = M[i - 1, w] + M[i - 1, w - weights[i]]
     1.3 Use minimum number of items:
         M[i, w] = min(M[i - 1, w], M[i - 1, w - weights[i]] + 1)
+
+2. Classical 0/1 Knapsack Problem:
+    each item has a value of v[i]
+    M[i, w] = max(M[i - 1, w], M[i - 1, w - weights[i]] + v[i])
+
+3. Unbounded Knapsack Problem:
+    M[i, w] = max(M[i - 1, w], M[i, w - weights[i]] + v[i])
+    
+4. Group Knapsack Problem:
+    M[k, w] = max(M[k - 1, w], M[k - 1, w - weights[i]] + v[i] | item i belongs to group k)
+
+5. 2D-weights Knapsack Problem:
+    M[i, w1, w2] = max(M[i - 1, w1, w2], M[i - 1, w1 - weights1[i], w2 - weights2[i]] + v[i]) 
+    
+*/
