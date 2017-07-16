be47172b2091d78a9c19dc7efe09f4ac750d2b5d
HungryOrc <shu.da.the119@gmail.com>
2017-07-15 23:18:42 -0400

    Update Backpack_Classic0or1_MaxTotalValue_ReturnTotalValue.java

--- Backpack_Classic0or1_MaxTotalValue_ReturnTotalValue.java
+++ Backpack_Classic0or1_MaxTotalValue_ReturnTotalValue.java
@@ -4,37 +4,40 @@
 
 /* 思路：二维 DP
 int dp[i][s] 的意思是：使用数组里index为0到i的items中的任意几个（可以一个也不选，也可以从中任选几个），
-最少用多少个items，能正好组成总 size = s。
+正好组成总 size = s 的前提下，最大可能的总 value 是多少。
 所以 int dp[][] = new int[number of items][capacity of the backpack + 1]。
+
 Base Cases:
 Base Case 1: 对于数组里的第一个item，
-    if(sizes[0] <= capacity)，canSumTo[0][sizes[0]] = 1
-    其他的 canSumTo[0][s != sizes[0]] 都 = 正无穷大，用这种方式来表示根本不可能完成
-Base Case 2: size和为0的情况，对于任何多个items，都是可以的！！ 即什么都不放 ！！ 所以都置为 0，即不放任何item
+    if(sizes[0] <= capacity)，canSumTo[0][sizes[0]] = values[0]
+    其他的 canSumTo[0][s != sizes[0]] 都 = 正无穷小，用这种方式来表示根本不可能完成
+Base Case 2: size和为0的情况，对于任何多个items，都是可以的！！ 即什么都不放 ！！ 所以都置为 0，即不放任何item，自然value和为 0
     for (int i = 0; i < n; i++)，canSumTo[i][0] = 0;
     
 Induction Rule:
-dp[i][s] = min(dp[i - 1][s], dp[i - 1][s - sizes[i]] + 1)
+dp[i][s] = max(dp[i - 1][s], dp[i - 1][s - sizes[i]] + values[i])
 其中 dp[i - 1][s] 的意思是：i之前的那些items已经可以组成总和正好为 s 的组合了，item i 不参与的话，自然也还是和为s的组合；
 dp[i - 1][s - sizes[i]] 的意思是：i之前的那些items组成了总和正好为 s - sizes[i] 的组合，那么 item i 参与进来后，自然正好就是和为s。
+
 Return: dp[n - 1][capacity of the backpack]
+
 Time: O(n * capacity), 其中n是items的个数
 Space: O(n * capacity)。可以优化为 O(capacity)，因为dp矩阵里，其实每一次loop只用一行就够了   */
 
 
 public class Solution {
      
-    public int backPack(int capacity, int[] sizes) {
+    public int backPack(int capacity, int[] sizes, int[] values) {
         int n = sizes.length;
         
         int[][] canSumTo = new int[n][capacity + 1];
         
         // base case 1
         for (int i = 0; i < n; i++) {
-            canSumTo[0][i] = Integer.MAX_VALUE;
+            canSumTo[0][i] = Integer.MIN_VALUE;
         }
         if (sizes[0] <= capacity) {
-            canSumTo[0][sizes[0]] = 1;
+            canSumTo[0][sizes[0]] = value[0];
         }
         
         // base case 2
@@ -49,8 +52,8 @@ public class Solution {
             for (int sum = 1; sum <= capacity; sum++) {
                 
                 if (sum - curItemSize >= 0 && // 别忘了检查越界 ！！！
-                    canSumTo[i - 1][sum - curItemSize] != Integer.MAX_VALUE) { // 如果等于正无限，就别再 +1 了 ！！！
-                    canSumTo[i][sum] = Math.min(canSumTo[i - 1][sum], canSumTo[i - 1][sum - curItemSize] + 1);
+                    canSumTo[i - 1][sum - curItemSize] != Integer.MIN_VALUE) { // 如果等于正无限，就别再 +1 了 ！！！
+                    canSumTo[i][sum] = Math.max(canSumTo[i - 1][sum], canSumTo[i - 1][sum - curItemSize] + values[i]);
                 } else {
                     canSumTo[i][sum] = canSumTo[i - 1][sum]; // 这种情况下就不加后面那项了 ！！！
                 }
