af5156c4a0732220ae03c25a89bb3fa34f1da49c
HungryOrc <shu.da.the119@gmail.com>
2017-07-26 15:22:46 -0400

    Update 2Sum_TwoNodesInBST_SumToTarget.java

--- 2Sum_TwoNodesInBST_SumToTarget.java
+++ 2Sum_TwoNodesInBST_SumToTarget.java
@@ -13,3 +13,67 @@ nodeRight一开始要指向整棵树的最大的node，即我们从root开始不
 按照这样一段一段往左走的方式，就把整棵树撕成一绺一绺地 从右上到左下的布条了 ！！！
 而这样做的过程中，stack里依次pop出来的nodes，它们的values，是从小到大的升序排列 ！！！  */
 
+public class Solution {
+
+    public boolean pairOFNodesExist() {
+        Stack<TreeNode> stackLToR = new Stack<>();
+        Stack<TreeNode> stackRToL = new Stack<>();
+      
+        boolean done1 = false, done2 = false;
+      
+        TreeNode nodeL = root, nodeR = root;
+        int val1 = 0, val2 = 0;
+        
+        while (true) {
+            // 操作左边
+            while (done1 == false) {
+                if (nodeL != null) {
+                    stackLToR.push(nodeL);
+                    nodeL = nodeL.left;
+                } else { 
+                    if (stackLToR.isEmpty()) {
+                        done1 = true;
+                    } else {
+                        nodeL = stackLToR.pop();
+                        val1 = nodeL.val;
+                        nodeL = nodeL.right;
+                        done1 = true;
+                    }
+                }
+            }
+            
+            // 操作右边
+            while (done2 == false) {
+                if (nodeR != null) {
+                    stackRToL.push(nodeR);
+                    nodeR = nodeR.right;
+                } else {
+                    if (stackRToL.isEmpty()) {
+                        doneR = true;
+                    } else {
+                        nodeR = stackRToL.pop();
+                        val2 = nodeR.val;
+                        nodeR = nodeR.left;
+                        doneR = true;
+                    }
+                }
+            }
+          
+            // 如果找到了答案
+            if ((valR != valL) && (valR + valL == target)) {
+                return true;
+            // 小于target，左node要向右走
+            } else if (valR + valL < target) {
+                doneL = false;
+            // 大于target，右node要向左走
+            } else if (valR + valL > target) {
+                doneR = false;
+            }
+          
+            // 提前终止
+            if (valL >= valR) {
+                return false;
+            }
+        }
+    }
+}
