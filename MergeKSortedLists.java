fc75b713a374318bb50512c5ceedba02c578bf04
HungryOrc <shu.da.the119@gmail.com>
2017-07-31 16:42:50 -0400

    Update MergeKSortedLists.java

--- MergeKSortedLists.java
+++ MergeKSortedLists.java
@@ -23,6 +23,7 @@ class PQNode implements Comparable<PQNode> {
     this.list = list;
   }
   
+  // class PQNode 内置的比较函数，int compareTo(PQNode anotherPQNode)
   @Override
   public int compareTo(PQNode node) {
     if (this.value > node.value)
@@ -43,27 +44,31 @@ public class Solution {
     
     PriorityQueue<PQNode> pq = new PriorityQueue<>();
     
-    // 先把每个list里的打头的ListNode转化成PQNode, 放到PriorityQueue里面去
+    // Step 1. 先把每个list里的打头的ListNode转化成PQNode, 放到PriorityQueue里面去
     for (int i = 0; i < listOfLists.size(); i++) {
       ListNode headListNodeInListI = listOfLists.get(i);
       pq.offer(new PQNode(headListNodeInListI.value, i));
+      
       // 别忘了！！！ in each list, point the head pointer of the list to the next ListNode
       listOfLists.set(i, headListNodeInListI.next);
     }
     
-    // get the head ListNode for the final result list that we'll return for this program
+    // Step 2. get the head ListNode for the final result list that we'll return for this program
     PQNode firstPQNode = pq.poll();
     ListNode headListNode = new ListNode(firstPQNode.value); // we'll eventually return this ListNode
     ListNode curListNode = headListNode;
     
+    // 把head list node 后面的 next node 放到 priority queue 里面去
     if (listOfLists.get(firstPQNode.list) != null) {
       pq.offer(new PQNode(listOfLists.get(firstPQNode.list).value, firstPQNode.list));
       // 别忘了！！！ in each list, point the head pointer of the list to the next ListNode
       listOfLists.set(firstPQNode.list, listOfLists.get(firstPQNode.list).next);
     }
     
+    // Step 3. 把k个lists都动起来
     while(!pq.isEmpty()) {
       PQNode curPQNode = pq.poll();
+      
       curListNode.next = new ListNode(curPQNode.value);
       curListNode = curListNode.next;
       
