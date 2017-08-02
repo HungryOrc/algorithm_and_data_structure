6e508741b62572a12bbbeb4171f368faed36be78
HungryOrc <shu.da.the119@gmail.com>
2017-08-02 07:55:34 -0400

    Update and rename RemoveAllTheNodesOutsideARangeInBinarySearchTree.java to RemoveAllNodesOutsideARangeInBinarySearchTree.java

--- RemoveAllNodesOutsideARangeInBinarySearchTree.java
+++ RemoveAllNodesOutsideARangeInBinarySearchTree.java
@@ -11,11 +11,13 @@
 特别注意一个node被删除以后，它下面的children往上接入它parent的情况 ！！！
 上面的例子中，
 3 因为小于4而被删除，3的左边的nodes一定会更小，也必然被删除，只有3的右边的nodes有可能被接到3的parent的下面，继承3曾经的左子身份
-11因为大于10而被杀出，11右边的nodes一定会更大，也必然被删除，只有11左边的nodes有可能被接到11的parent的下面，继承11曾经的右子身份  */
+11因为大于10而被删除，11右边的nodes一定会更大，也必然被删除，只有11左边的nodes有可能被接到11的parent的下面，继承11曾经的右子身份  */
 
 // 代码虽然短，但思路非常精妙 ！！！ 要好好理解 ！！！
+
 public class Solution {
-    // lb (lower bound) < ub (upper bound)
+        
+    // Assumption: lb (lower bound) < ub (upper bound)
     public TreeNode removeNodesOutsideRange(TreeNode root, int lb, int ub) {
         if (root == null) {
             return null;
