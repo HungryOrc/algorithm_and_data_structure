416f6a40ca0e8e202d8d2523ef2f0d0425e9d97f
HungryOrc <shu.da.the119@gmail.com>
2017-08-04 10:52:44 -0400

    Update and rename KthLargestElementInAnUnsortedArray.java to KthLargestElementInUnsortedArray_QuickSelect.java

--- KthLargestElementInUnsortedArray_QuickSelect.java
+++ KthLargestElementInUnsortedArray_QuickSelect.java
@@ -4,7 +4,9 @@ For example, Given [3,2,1,5,6,4] and k = 2, return 5.
 
 Note: You may assume k is always valid, 1 ≤ k ≤ array's length. */
 
-// Quick Select 方法
+
+// 方法1：Quick Select
+// 时间：O(n)
 public class Solution {
     
     public int findKthLargest(int[] nums, int k) {
@@ -63,3 +65,11 @@ public class Solution {
         nums[i2] = tmp;
     }
 }
+
+
+// 方法2：把元素都放到 max heap 里面去，最后从heap里pop出来 k 个
+// 时间：O(n logn)，这种方法过度处理了
+
+
+// 方法3：排序整个数组，从小到大。最后取后面的k个
+// 时间：O(n logn)，这种方法过度处理了
