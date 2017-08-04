d01112bf3a0c6daf6aa0c317ad688ce47cddf0e5
HungryOrc <shu.da.the119@gmail.com>
2017-08-04 11:27:41 -0400

    Update KthLargestElementInUnsortedArray_QuickSelect.java

--- KthLargestElementInUnsortedArray_QuickSelect.java
+++ KthLargestElementInUnsortedArray_QuickSelect.java
@@ -6,6 +6,7 @@ Note: You may assume k is always valid, 1 ≤ k ≤ array's length. */
 
 
 // 方法1：Quick Select
+// quick select 和 quick sort 的区别在于，quick select只排序我们要的那一半，不管另一半
 // 时间：平均来说是 O(n)。最差可能是 O(n^2)，但可能性很低，因为那需要大多数的pivot都选成了最大或者最小
 public class Solution {
     
@@ -18,7 +19,6 @@ public class Solution {
         return quickSelect_PthMin(nums, 0, nums.length - 1, nums.length - k + 1);
     }
     
-    // quick select 和 quick sort 的区别在于，quick select只排序我们要的那一半，不管另一半
     // 最后一个参数 p 的意思是，找第 p 小的数
     private int quickSelect_PthMin(int[] nums, int start, int end, int p) {
         
@@ -26,7 +26,7 @@ public class Solution {
         // 在后面的partition里，pivot的选取是选数组里的最右边的数
         int chosenIndex = partition(nums, start, end);
         
-        if (chosenIndex < p - 1) { // 注意！！第 p 小的数的 index 是 p - 1 ！！！
+        if (chosenIndex < p - 1) { // 注意！！第 p 小的数的 index 是 p - 1 ！！
             return quickSelect_PthMin(nums, chosenIndex + 1, end, p);
         } else if (chosenIndex > p - 1) {
             return quickSelect_PthMin(nums, start, chosenIndex - 1, p);
@@ -38,7 +38,7 @@ public class Solution {
     // 和一般的 quick sort 的partition一样
     private int partition(int[] nums, int start, int end) {
         int left = start;
-        int right = end - 1; // 别忘了 - 1 ！！因为 pivot取为最后一个数了！
+        int right = end - 1; // 别忘了 - 1！因为 pivot取为最后一个数了！所以right只能取为倒数第二个！
         int pivot  = nums[end];
         
         while (left <= right) {
@@ -48,13 +48,14 @@ public class Solution {
             while (left <= right && nums[right] >= pivot) {
                 right --;
             }
-            if (left < right) {
+            if (left < right) { // 这里不再要求<=，而只要求< ！！这是为了避免left和right交叉之后中间再隔着一个 ！！！
                 swap(nums, left, right);
                 left ++;
                 right --;
             }
         }
         
+        // 最后left要么是紧贴着right并且在right的右边一位，要么是left和right重合
         swap(nums, left, end);
         return left;
     }
