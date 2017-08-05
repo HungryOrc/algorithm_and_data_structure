586b1e4953024ed349ead3ec75ef63d3a1a6306e
HungryOrc <shu.da.the119@gmail.com>
2017-08-05 17:10:00 -0400

    Update KthSmallestElementInASortedMatrix.java

--- KthSmallestElementInASortedMatrix.java
+++ KthSmallestElementInASortedMatrix.java
@@ -95,34 +95,33 @@ public class Solution {
 }
 
 
-// 方法2：用Binary Search做。我没有想到。很巧妙 ！！！
+// 方法2：用Binary Search做。最左上角一定时 global min，最右下角一定是 global max。我没有想到。很巧妙 ！！！
 // Ref: https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/
-public class Solution 
-{
-    public int kthSmallest(int[][] matrix, int k) 
-    {
+
+public class Solution {
+   
+    public int kthSmallest(int[][] matrix, int k) {
         int rows = matrix.length;
         int cols = matrix[0].length;
        
         int lowValue = matrix[0][0];
         int highValue = matrix[rows - 1][cols - 1];
         
-        while(lowValue < highValue) 
-        {
+        while(lowValue < highValue) {
             int mid = lowValue + (highValue - lowValue) / 2;
 
             // 对每一个mid值，算一下，在matrix里，有多少个数小于或者等于这个mid值
             int count = 0; 
            
+            // 从上到下，逐行，找这一行里 > mid 的数有多少个
             int j = cols - 1;
-            // 从上到下，逐行，去掉这一行里 > mid 的数
             for(int i = 0; i < rows; i++) {
                 while(j >= 0 && matrix[i][j] > mid) {
                     j--;
                 }
                 count += (j + 1);
             }
-            /* 上面这段也可以换做：从左到右，逐列，去掉这一列里 > mid 的数
+            /* 上面这段也可以换做：从左到右，逐列，找这一列里 > mid 的数有多少个
             int i = rows - 1;
             for(int j = 0; j < cols; j++) {
                 while(i >= 0 && matrix[i][j] > mid) {
@@ -132,14 +131,15 @@ public class Solution
             } */
             
             // 对于当前的mid值来说，count算出来了。现在把count和k做对比
-            if (count < k) 
+            if (count < k) {
                 lowValue = mid + 1;
-            else if (count >= k)
+            } else { // count >= k
                 highValue = mid;
-            /* 注意！！！上面这段 if else 水特别深 ！！！
+            }
+            /* 注意！！！
             >= k 时，不要 mid-1，只要mid ！！！
             == k 时，不要马上 return mid，而是非要等到最后 high 和 low重合再return！！！这样的目的是
-            找到实际存在于matrix里的第k个数！！！！！而非某个大于matrix里的第k个数而小于matrix里的第k+1个数 的数 ！！！！！ */
+            找到实际存在于matrix里的第k个数！！！！！而非某个大于matrix里的第k个数而小于matrix里的第k+1个数 的数！！！ */
         }
         // 最后return highValue或者lowValue都是一样的
         // 因为按照此处二分法的实施方式，它们两最后一定相等
