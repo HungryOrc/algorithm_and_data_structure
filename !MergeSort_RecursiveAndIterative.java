efc5d3fe5f4f3a779e2b04ab9b6254c2af7b8938
HungryOrc <shu.da.the119@gmail.com>
2017-08-08 11:37:03 -0400

    Update and rename !MergeSort.java to !MergeSort_RecursiveAndIterative.java

--- !MergeSort_RecursiveAndIterative.java
+++ !MergeSort_RecursiveAndIterative.java
@@ -1,4 +1,5 @@
-// 经典模板
+// 经典模板。包括 Recursive 和 Iterative 两种方式
+
 // Time: O(n*logn)
 // Space: O(n)，因为根据 Recursion Tree，每一层 call stack 都新建了 helper array，一共有 logn 层call stack，
 // 各个call stack所新建的 helper array的长度分别是 n/2, n/4, n/8... 2, 1
@@ -18,6 +19,10 @@ public class MergeSort {
     return array;
   }
   
+  // 核心的 merge sort 函数
+  // ---------------------------------------------------------------------------------------
+  
+  // 方式1：Recursive
   private void mergeSort(int[] array, int[] helperArray, int start, int end) {
     if (start >= end) { // when there is only 1 element or 0 element
       return;
@@ -29,6 +34,38 @@ public class MergeSort {
     
     merge(array, helperArray, start, mid, end);
   }
+  
+  // 方式2：Iterative
+  // http://www.geeksforgeeks.org/iterative-merge-sort/
+  /* Iterative mergesort function to sort arr[0...n-1] */
+  private void mergeSort(int arr[], int n) {
+     int curr_size;  // For current size of subarrays to be merged
+                     // curr_size varies from 1 to n/2
+     int left_start; // For picking starting index of left subarray
+                     // to be merged
+
+     // Merge subarrays in bottom up manner.  First merge subarrays of
+     // size 1 to create sorted subarrays of size 2, then merge subarrays
+     // of size 2 to create sorted subarrays of size 4, and so on.
+     for (curr_size=1; curr_size<=n-1; curr_size = 2*curr_size)
+     {
+         // Pick starting point of different subarrays of current size
+         for (left_start=0; left_start<n-1; left_start += 2*curr_size)
+         {
+             // Find ending point of left subarray. mid+1 is starting 
+             // point of right
+             int mid = left_start + curr_size - 1;
+
+             int right_end = min(left_start + 2*curr_size - 1, n-1);
+
+             // Merge Subarrays arr[left_start...mid] & arr[mid+1...right_end]
+             merge(arr, left_start, mid, right_end);
+         }
+     }
+  }
+  
+  
+  // ---------------------------------------------------------------------------------------
   
   private void merge(int[] array, int[] helperArray, int start, int mid, int end) {
     if (start >= end) { // when there is only 1 element or 0 element
