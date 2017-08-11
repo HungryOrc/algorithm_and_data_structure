e3d9dc1cb0adf765b7a9377ed45b40d8ae897fb3
HungryOrc <shu.da.the119@gmail.com>
2017-08-10 23:44:19 -0400

    Update CustomIteratorToGetAllTheOddNumbersInAList.java

--- CustomIteratorToGetAllTheOddNumbersInAList.java
+++ CustomIteratorToGetAllTheOddNumbersInAList.java
@@ -14,6 +14,8 @@ public void oddIterator(List<Integer> list) {
 
 现在要求做的，是自定义一个class，来实现这个专门挑奇数的Iterator   */
 
+
+// 方法1：不采用list自带的默认iterator。代码简明一些。缺点是会直接接触 raw data，就是原list，没有做到很好的防火墙隔离
 public class OddIterator {
     List<Integer> list;
     int curIndex;
@@ -40,6 +42,47 @@ public class OddIterator {
             return result;
         }
         
+        return null;
+    }
+}
+
+
+// 方法2：使用list自带的默认iterator，在这个基础上构建我们的iterator，主要的好处是不直接接触原list，安全性高
+public class OddIterator {
+    List<Integer> list;
+    Iterator<Integer> iter;
+    // 因为将会使用list自带的iterator，所以不再设置 int curIndex ！！！
+    // 不过专门设了一个 cur value，如下。具体作用看后面代码
+    Integer curValue;
+
+    // constructor
+    public OddIterator(List<Integer> input) {
+        list = input;
+        iter = input.Iterator();
+        curValue = null; // 这个不写也是默认null，写是为了更清楚
+    }
+    
+    public boolean hasNext() {
+        if (curValue != null) { // 重要 ！！！
+            return true;
+        }
+        
+        while (iter.hasNext()) {
+            Integer nextValue = iter.next();
+            if (nextValue % 2 == 1) {
+                curValue = nextValue;
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public Integer next() {
+        if (this.hasNext()) {
+            Integer result = curValue;
+            curValue = null; // 别忘了这个 ！！！
+            return result;
+        }
         return null;
     }
 }
