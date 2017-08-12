0843dde5da7182db44515ab9751862324b31d4d2
HungryOrc <shu.da.the119@gmail.com>
2017-08-12 08:58:27 -0400

    Update CustomIteratorToGetOneNumberInEveryKNumbers.java

--- CustomIteratorToGetOneNumberInEveryKNumbers.java
+++ CustomIteratorToGetOneNumberInEveryKNumbers.java
@@ -4,24 +4,24 @@ public class JumpIterator {
     List<Integer> list;
     Iterator<Integer> iter;
     // 因为将会使用list自带的iterator，所以不再设置 int curIndex ！！！
-    // 不过专门设了一个 cur value，如下。具体作用看后面代码
-    Integer curValue;
+    // 不过专门设了一个 next value，如下。具体作用看后面代码
+    Integer nextValue;
     int k; // 每k个数里面取1个
 
     // constructor
     public JumpIterator(List<Integer> input, int k) {
         list = input;
         iter = input.Iterator();
-        curValue = null; // 这个不写也是默认null，写是为了更清楚
+        nextValue = null; // 这个不写也是默认null，写是为了更清楚
         this.k = k;
     }
     
     public boolean hasNext() {
-        if (curValue != null) { // 重要 ！！！
+        if (nextValue != null) { // 重要 ！！！
             return true;
         }
         
-        Integer tmp = null; // 关键 ！！！ 这里要再设一个变量，作为 curValue 的前锋 ！！！ 下面这些代码都很重要
+        Integer tmp = null; // 关键 ！！！ 这里要再设一个变量，作为 nextValue 的前锋 ！！！ 下面这些代码都很重要
         for (int i = 0; i < this.k; i++) {
             if (iter.hashNext()) {
                 tmp = iter.next();
@@ -29,14 +29,14 @@ public class JumpIterator {
                 return false;
             }
         }
-        curValue = tmp;
+        nextValue = tmp;
         return true;
     }
     
     public Integer next() {
         if (this.hasNext()) {
-            Integer result = curValue;
-            curValue = null; // 别忘了这个 ！！！
+            Integer result = nextValue;
+            nextValue = null; // 别忘了这个 ！！！
             return result;
         }
         return null;
