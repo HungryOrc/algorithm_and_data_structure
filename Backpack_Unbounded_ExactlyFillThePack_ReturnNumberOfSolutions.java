5b0adc98831d443963c94435e8e494a6eef9c019
HungryOrc <shu.da.the119@gmail.com>
2017-08-18 11:43:17 -0400

    Update Backpack_Unbounded_ExactlyFillThePack_ReturnNumberOfSolutions.java

--- Backpack_Unbounded_ExactlyFillThePack_ReturnNumberOfSolutions.java
+++ Backpack_Unbounded_ExactlyFillThePack_ReturnNumberOfSolutions.java
@@ -14,17 +14,36 @@ Base Case 1: 对于数组里的第一个item，
 Base Case 2: size和为0的情况，对于任何多个items，都是可以的！！ 即什么都不放 ！！ 所以都置为1，即1种方法
     for (int i = 0; i < n; i++)，dp[i][0] = 1;
     
-Induction Rule: 注意！这一题的递推公式比较特别 ！！          
+Induction Rule: 这里归纳出来两种递归规则，即两种方法
+
+方法1：          
 for (int j = 0; j <= size / sizes[i]; j++) {
     dp[i][size] += dp[i - 1][size - j * sizes[i]];
 }
-             
+
+方法2：
+if (curValue > sum) {
+    dp[i][sum] = dp[i - 1][sum];
+} else {
+    // 这里分两种情况，
+    // 加号左边 dp[i - 1][sum] 表示，sum里面将没有array[i]的任何参与，
+    // 加号右边 dp[i][sum - curValue] 表示，sum里面将存在array[i]的一次或者多次参与，
+    // 特别注意 ！！！ 
+    // dp[i][sum - curValue] 是包含了 dp[i - 1][sum - curValue] 的 ！！！ 所以下面的式子不要再加 dp[i - 1][sum - curValue] ！！！
+    // 因为：
+    // dp[i - 1][sum - curValue] 表示 array[i] 已经被使用0次，然后将会被使用且仅使用一次 ！！！
+    // dp[i][sum - curValue] 表示 array[i] 已经被使用 0次，1次，或多次了，然后将再被使用一次 ！！！
+    
+    dp[i][sum] = dp[i - 1][sum] + dp[i][sum - curValue];
+}
+
 Return: dp[n - 1][capacity]
         
 Time: O(n * capacity), 其中n是items的个数
 Space: O(capacity)   */
 
 
+// 方法1：
 public class Solution {
 
     public int backPackIV(int[] sizes, int capacity) {
@@ -56,5 +75,45 @@ public class Solution {
         }
         
         return dp[n - 1][capacity];
+    }
+}
+
+
+// 方法2：
+public class Solution {
+    
+    public static long makeChange(int[] coins, int money) {
+        if (coins == null || coins.length == 0 || money < 0) {
+            return 0L;
+        }
+        
+        int n = coins.length;
+        long[][] dp = new long[n][money + 1];
+        
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = 1L;
+        }
+        
+        if (coins[0] <= money) {
+            int sum = coins[0];
+            while (sum <= money) {
+                dp[0][sum] = 1L;
+                sum += coins[0];
+            }
+        }
+        
+        for (int i = 1; i < n; i++) {
+            for (int sum = 1; sum <= money; sum++) {
+                int curValue = coins[i];
+                
+                if (curValue > sum) {
+                    dp[i][sum] = dp[i - 1][sum];
+                } else {
+                    dp[i][sum] = dp[i - 1][sum] + dp[i][sum - curValue];
+                }
+            }
+        }
+        
+        return dp[n - 1][money];
     }
 }
