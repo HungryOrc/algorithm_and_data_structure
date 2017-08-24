169d8f1d80ae44babc951318905c3635782950da
HungryOrc <shu.da.the119@gmail.com>
2017-08-24 17:23:41 -0400

    Update LongestPhrasesInATweet.java

--- LongestPhrasesInATweet.java
+++ LongestPhrasesInATweet.java
@@ -2,5 +2,34 @@
 就像一个sub array，要满足：它们的总长度 <= k，然后这些单词的总个数最大 */
 
 public class Solution {
-
+    
+    public int maxWords (int[] wordLengths, int k) {
+        // ignore the corner cases
+      
+        int maxNumOfWords = 0;
+        int totalLenOfWords = 0;
+      
+        Queue<Integer> queueOfLens = new LinkedList<>();
+      
+        for (int len : wordLengths) {
+            
+            if (totalLenOfWords + len <= k) {
+                queueOfLens.offer(len);
+                totalLenOfWords += len;
+            }
+            else { // totalLenOfWords + len > k
+                maxNumOfWords = Math.max(maxNumOfWords, queueOfLens.size());
+                
+                while (totalLenOfWords + len > k) {
+                    totalLenOfWords -= queueOfLens.poll();
+                }
+              
+                totalLenOfWords += len;
+                queueOfLens.offer(len);
+                maxNumOfWords = Math.max(maxNumOfWords, queueOfLens.size());
+            }
+        }
+        maxNumOfWords = Math.max(maxNumOfWords, queueOfLens.size());
+        return maxNumOfWords;
+    }
 }
