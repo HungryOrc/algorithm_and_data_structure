2b22188a451b5797bc1a07f5096e39bd9f68ae18
HungryOrc <shu.da.the119@gmail.com>
2017-08-29 08:30:02 -0400

    Update StrStr_WithAStar.RabinKarp.java

--- StrStr_WithAStar.RabinKarp.java
+++ StrStr_WithAStar.RabinKarp.java
@@ -4,8 +4,8 @@
 
 public class Solution {
 
-	public final int BASE = 1000000;
-	public final int HASH_MAGIC_NUMBER = 31;
+    public final int BASE = 1000000;
+    public final int HASH_MAGIC_NUMBER = 31;
 	
     public int strStr_WithStart(String source, String target) {
     	if (source == null || target == null) {
@@ -16,53 +16,34 @@ public class Solution {
     	
     	int n = source.length();
     	int m = target.length();
-    	
-    	// *
-    	System.out.println("m: " + m);
-    	
+
     	// 算出 31^m，每次去掉最左边的字母的时候要用
     	int topPower = 1;
     	for (int i = 0; i < m; i++) {
-    		topPower = (topPower * HASH_MAGIC_NUMBER) % BASE;
+    	    topPower = (topPower * HASH_MAGIC_NUMBER) % BASE;
     	}
-    	
-    	// *
-    	System.out.println("topPower: " + topPower);
-    	
-    	
+
     	// 计算 target 的 Hash Code。注意要刨掉 * 那一位 ！
     	int targetHashCode = 0;
     	int starDigit = 0; // * 所在的 index
     	
-
-    	
     	for (int i = 0; i < m; i++) {
     		if (target.charAt(i) == '*') {
-    			starDigit = i;
-    			// 注意 ！进一位还是不能少的 ！
-    			targetHashCode = (targetHashCode * HASH_MAGIC_NUMBER) % BASE;
+    		    starDigit = i;
+    		    
+		    // 注意 ！进一位还是不能少的 ！
+    		    targetHashCode = (targetHashCode * HASH_MAGIC_NUMBER) % BASE;
     		} else {
-    			targetHashCode = (targetHashCode * HASH_MAGIC_NUMBER + target.charAt(i)) % BASE;
-    			
-    			System.out.println("i: " + i + ", charAt(i): " + target.charAt(i) +
-    					", tmp hash code: " + targetHashCode);
+    		    targetHashCode = (targetHashCode * HASH_MAGIC_NUMBER + target.charAt(i)) % BASE;
     		}
     	}
-    	
-    	// *
-    	System.out.println("starDigit: " + starDigit);
-    	System.out.println("targetHashCode: " + targetHashCode);
-    	
+
     	// 计算 * 所在的那一位的 hashCode 的乘数
     	int starPower = 1;
     	for (int i = 0; i < m - 1 - starDigit; i++) {
     		starPower = (starPower * HASH_MAGIC_NUMBER) % BASE;
     	}
-    	
-    	// *
-    	System.out.println("starPower: " + starPower);
-    	
-    	
+
     	// 逐个检查source里每一段substring的 Hash Code
     	int hashCode = 0;
     	
@@ -80,6 +61,8 @@ public class Solution {
             if (i >= m) {
                 hashCode -= (source.charAt(i - m) * topPower) % BASE; 
 
+		// 减出来的结果可能 < 0
+		// 那么需要用下面的小trick把hashCode变回成正数
                 if (hashCode < 0) {
                     hashCode += BASE;
                 }
@@ -89,32 +72,26 @@ public class Solution {
             // 如果长度正好是 m（此时即 i == m - 1），或者长度超过 m，
             // 则都要把 * 所在的那一位所对应的字母所贡献的hashCode消除
             if (i >= m - 1) {
-	            int posOfStar = i - m + 1 + starDigit;
-	            
-	            
-	            int tmpHashCode = hashCode - (source.charAt(posOfStar) * starPower) % BASE;
-	            // 减出来的结果可能 < 0
-	            // 那么需要用下面的小trick把hashCode变回成正数
-	            if (tmpHashCode < 0) {
-	            	tmpHashCode += BASE;
-	            }
+		// index of the slot of * in the source string
+	        int posOfStar = i - m + 1 + starDigit;
+    
+	        int tmpHashCode = hashCode - (source.charAt(posOfStar) * starPower) % BASE;
+	       
+	        if (tmpHashCode < 0) {
+	            tmpHashCode += BASE;
+	        }
             
-	            
-	         // *
-	        	System.out.println("i: " + i + ", tmpHashCode: " + tmpHashCode);
-	        	
-	        	
-	            // Step 4, double check, in case there was a HashCode Conflict,
-	            // meaning that different Strings had the same HashCode
-	        	// 注意 ！ 这里要分别判断被 * 隔开的两段，是否分别相等 ！
-	            if (tmpHashCode == targetHashCode) {
-	            	int startIndexAtSource = i - m + 1;
-	                // substring的右端是不包含的，所以得 +1
-	                if (source.substring(startIndexAtSource, startIndexAtSource + starDigit).equals(target.substring(0, starDigit)) &&
-	                	source.substring(startIndexAtSource + starDigit + 1, i + 1).equals(target.substring(starDigit + 1, m))) {
-	                    return i - m + 1; // the starting index
-	                }
+	        // Step 4, double check, in case there was a HashCode Conflict,
+	        // meaning that different Strings had the same HashCode
+	        // 注意 ！ 这里要分别判断被 * 隔开的两段，是否分别相等 ！
+	        if (tmpHashCode == targetHashCode) {
+	            int startIndexAtSource = i - m + 1;
+	            // substring的右端是不包含的，所以得 +1
+	            if (source.substring(startIndexAtSource, startIndexAtSource + starDigit).equals(target.substring(0, starDigit)) &&
+	                source.substring(startIndexAtSource + starDigit + 1, i + 1).equals(target.substring(starDigit + 1, m))) {
+	                return i - m + 1; // the starting index
 	            }
+	        }
             }
         }
         return -1;
