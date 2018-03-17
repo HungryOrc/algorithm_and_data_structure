2998ccdf579656e4585773785e1100340710c591
HungryOrc <shu.da.the119@gmail.com>
2018-03-17 18:21:07 -0400

    Update CountOfSmallerNumbersAfterSelfInAnArray.java

--- CountOfSmallerNumbersAfterSelfInAnArray.java
+++ CountOfSmallerNumbersAfterSelfInAnArray.java
@@ -2,10 +2,6 @@
 The counts array has the property where counts[i] is the number of smaller elements to the right of nums[i].
 
 Example: Given nums = [5, 2, 6, 1]
-To the right of 5 there are 2 smaller elements (2 and 1).
-To the right of 2 there is only 1 smaller element (1).
-To the right of 6 there is 1 smaller element (1).
-To the right of 1 there is 0 smaller element.
 Return the array [2, 1, 1, 0]. */
 
 // 很巧妙的方法！
