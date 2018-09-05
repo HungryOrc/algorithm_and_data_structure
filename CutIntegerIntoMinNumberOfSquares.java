58da477413e1cb32e3cacb63cc4b246274c07e6a
HungryOrc <shu.da.the119@gmail.com>
2018-09-04 23:43:15 -0400

    Update CutIntegerIntoMinNumberOfSquares.java

--- CutIntegerIntoMinNumberOfSquares.java
+++ CutIntegerIntoMinNumberOfSquares.java
@@ -32,3 +32,22 @@ public class Solution {
         return dp[num];
     }
 }
+Time: O(n^2)
+
+// 下面这个DP更快！！！ 把两个都留着！然后在leetcode里测试时间消耗！！
+public int numOfSquares(int n) {
+    if (n <= 0) {
+        return 0;
+    }
+	
+    int[] dp = new int[n + 1];
+    for (int i = 1; i <= n; i++) {
+	dp[i] = i;
+        for (int i = 1; j * j <= i; j++) {    
+            dp[i] = Math.min(dp[i], 1 + dp[i - j * j]);
+	}
+    }
+    return dp[n];
+}
+
+Time: O(n^1.5)
