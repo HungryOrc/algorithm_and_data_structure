7e99ede93f2330688b2301b3c9f819ebfe73c684
HungryOrc <shu.da.the119@gmail.com>
2018-10-20 12:53:08 -0400

    Update Permutations_NoDuplicate.java

--- Permutations_NoDuplicate.java
+++ Permutations_NoDuplicate.java
@@ -1,39 +1,31 @@
   
   
-    // 方法2：Recursion。DFS的套路
+    // 方法2：DFS Recursion
     // Ref: http://www.jiuzhang.com/solutions/permutations/
     public List<List<Integer>> permute(int[] nums) {
-        ArrayList<List<Integer>> result = new ArrayList<List<Integer>>();
-        // nums 为空时，结果是空的 List of Lists
-        // nums 不为空但长度为0时，结果是长度为 1 的List of Lists，其内容为一个空List
-        if (nums == null) {
-            return result;
-        }
-        else if (nums.length == 0) {
-            result.add(new ArrayList<Integer>());
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null || nums.length == 0) {
             return result;
         }
         
         boolean[] visited = new boolean[nums.length]; // 默认都是false
-        findPermutations(nums, new ArrayList<Integer>(), visited, result);
+        dfs(nums, new ArrayList<Integer>(), visited, result);
         return result;
     }
   
-    private void findPermutations(int[] nums, ArrayList<Integer> curList, 
-                                  boolean[] visited, ArrayList<List<Integer>> result) {
+    private void dfs(int[] nums, List<Integer> curList, 
+                     boolean[] visited, List<List<Integer>> result) {
       
         if (curList.size() == nums.length) {
-            // 注意！！一定要复制！！
-            // 因为 curList 在其他分支里还会被反复利用！！不是到这里就使命终结了！！
+            // 一定要复制！
+            // 因为 curList 在其他分支里还会被反复利用！不是到这里就使命终结了
             result.add(new ArrayList<Integer>(curList));
             return;
         }
               
-        // 整个算法的精华在这里！每一次都是试图把整个数组里的每一个数都轮流放进去！！！
+        // 关键在此。每一次都是试图把整个数组里的每一个数都轮流放进去！
         for (int i = 0; i < nums.length; i++) {
-            
             if (visited[i] == false) {
-                
                 curList.add(nums[i]);
                 visited[i] = true;
               
@@ -46,19 +38,7 @@
     }
   
   
-    // 方法3：Non-Recursion 方法
-    // Ref: https://discuss.leetcode.com/topic/6377/my-ac-simple-iterative-java-python-solution
-    /* To permute n numbers, we can add the n-th number into the resulting List<List<Integer>> composed by the 
-     1st, 2nd, 3rd ... till the (n-1)th numbers, in every possible position.
-     For example, if the input num[] is {1,2,3}. 
-     (i) Add 1 into the initial List<List<Integer>> (let's call it "answer").
-     (ii) 2 can be added in front or after 1. So we have to copy the List<Integer> in answer (it's just {1}), then
-         add 2 in position 0 of {1}; then copy the original {1} again, and add 2 in position 1. 
-         Now we have an answer of {{2,1},{1,2}}. There are 2 lists in the current answer.
-     (iii) Now we have to add 3. First copy {2,1} and {1,2}, add 3 in position 0; 
-         then copy {2,1} and {1,2}, and add 3 into position 1; 
-         then do the same thing for position 3.
-     Finally we have 2*3=6 lists in answer, which is what we want. */
+
     public List<List<Integer>> permute(int[] nums) {
       
         List<List<Integer>> results = new ArrayList<>();
@@ -100,7 +80,7 @@
     
     
     // 方法4：Non-Recursion 方法，九章版本
-    // Ref: http://www.jiuzhang.com/solutions/permutations/
+    // 
     public List<List<Integer>> permute(int[] nums) {
       
         List<List<Integer>> permutations = new ArrayList<>();
