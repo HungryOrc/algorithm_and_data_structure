54265390c596a56bc1800a4d187cd01d53df2c52
HungryOrc <shu.da.the119@gmail.com>
2018-11-11 00:29:24 -0500

    Update BinaryTreeDepth_Maximum.java

--- BinaryTreeDepth_Maximum.java
+++ BinaryTreeDepth_Maximum.java
@@ -11,6 +11,8 @@ The maximum depth is the number of nodes along the longest path from the root no
  
 public class MaxDepthOfBinaryTree {
 
+    // 默写！
+ 
     // 方法1：Recursion - Divide and Conquer 分治法
     // 速度：按理说Recursion类方法的速度会偏慢，但实测相当快，leetcode的测试用例是1ms跑完
     public int maxDepth_ByRecursion(TreeNode root)
@@ -22,6 +24,53 @@ public class MaxDepthOfBinaryTree {
     }
  
  
+
+    
+ 
+ 
+    // 改写！默写！
+ 
+    // 方法 2：Breadth First Search (BFS)。使用了1个Queue，放"当前Depth"层所含有的所有Nodes
+    // 每次取出一个第m层的Node，就把它的子Node(s)(在第m+1层)放到Queue尾去。注意Queue是后进后出
+    // 取出的Node就再也不存入了。处理完一层即一个Depth再处理下一层
+    // 速度：很快
+    // Reference: https://discuss.leetcode.com/topic/33826/two-java-iterative-solution-dfs-and-bfs/2
+    public int maxDepth_ByBFS(TreeNode root)
+    {
+        if(root == null)
+            return 0;
+        
+        // 注意 Java 里的 Queue 只是一个 Interface，不能被直接实例化！
+        // 只能借助于一些implements Queue 的数据类型来进行实例化，包括 LinkedList：
+        Queue<TreeNode> nodeQueue = new LinkedList<>();
+        nodeQueue.offer(root);
+        
+        int curDepth = 0;
+        
+        while (!nodeQueue.isEmpty())
+        {
+            int numOfRemainingNodesInCurDepth = nodeQueue.size();
+            
+            while (numOfRemainingNodesInCurDepth > 0)
+            {
+                TreeNode curNode = nodeQueue.poll();
+                
+                if (curNode.left != null)
+                    nodeQueue.offer(curNode.left);
+                if (curNode.right != null)
+                    nodeQueue.offer(curNode.right);
+                    
+                numOfRemainingNodesInCurDepth --;
+            }
+            
+            curDepth ++;
+        }
+        
+        return curDepth;
+    }
+ 
+ 
+     // 蛋疼，抄一下就行了
  
     // 方法3：Depth First Search (DFS)。使用了2个Stack，一个存Node，一个存正对应于当前Node的Depth
     // Node的Stack：每次先pop出父Node，再push进它的子Node(s)。
@@ -62,46 +111,6 @@ public class MaxDepthOfBinaryTree {
         }
         
         return maxDepth;
-    }
-    
- 
-    // 方法4：Breadth First Search (BFS)。使用了1个Queue，放"当前Depth"层所含有的所有Nodes
-    // 每次取出一个第m层的Node，就把它的子Node(s)(在第m+1层)放到Queue尾去。注意Queue是后进后出
-    // 取出的Node就再也不存入了。处理完一层即一个Depth再处理下一层
-    // 速度：很快
-    // Reference: https://discuss.leetcode.com/topic/33826/two-java-iterative-solution-dfs-and-bfs/2
-    public int maxDepth_ByBFS(TreeNode root)
-    {
-        if(root == null)
-            return 0;
-        
-        // 注意 Java 里的 Queue 只是一个 Interface，不能被直接实例化！
-        // 只能借助于一些implements Queue 的数据类型来进行实例化，包括 LinkedList：
-        Queue<TreeNode> nodeQueue = new LinkedList<>();
-        nodeQueue.offer(root);
-        
-        int curDepth = 0;
-        
-        while (!nodeQueue.isEmpty())
-        {
-            int numOfRemainingNodesInCurDepth = nodeQueue.size();
-            
-            while (numOfRemainingNodesInCurDepth > 0)
-            {
-                TreeNode curNode = nodeQueue.poll();
-                
-                if (curNode.left != null)
-                    nodeQueue.offer(curNode.left);
-                if (curNode.right != null)
-                    nodeQueue.offer(curNode.right);
-                    
-                numOfRemainingNodesInCurDepth --;
-            }
-            
-            curDepth ++;
-        }
-        
-        return curDepth;
     }
 
 }
