e724a64e0037c90709f9e6c29178852f97a96cbc
HungryOrc <shu.da.the119@gmail.com>
2018-11-24 22:37:11 -0500

    Update !Trie_ImplementWithHashMap.java

--- !Trie_ImplementWithHashMap.java
+++ !Trie_ImplementWithHashMap.java
@@ -1,25 +1,3 @@
-/* 本 Trie（每个node里存的value是一个char）要实现的操作：
-   > 在每个 Trie Node 上存一个 int size，记录从 Trie root 开始到本node为止组成的这个prefix，后面一共有多少个word
-     如果到本node为止，恰好形成一个完整的word，那么这个word也要算到这个 size 里去
-   > 插入一个 String
-   > 查找一个 String 是否在Trie里作为一个完整的word
-   > 查找一个 Prefix String 是否存在于Trie里且返回其最后一个node，如果false，则返回null
-   > 返回一个 Prefix String 在Trie里的所有 后继(Subsequent) Strings，用 List<String> 来表示
-*/
-
-// Trie Node class
-class Node {
-    char value;
-    HashMap<Character, Node> children;
-    boolean endOfWord; // 注意！end of word以后，后面还可能继续有别的word！例：Trie里同时存 car 和 card 的情况
-    int size; // number of all descendants, including this node itself if it is an end of a word
-    
-    public Node(char c) {
-        this.value = c;
-        this.children = new HashMap<>();
-    }
-}
-
 public class Trie {
     Node root;
 
