d09f7b633eec5fa7c6f65038da108a988dce52b3
HungryOrc <shu.da.the119@gmail.com>
2018-12-04 23:41:16 -0500

    Update Backpack_Classic0or1_MaxTotalValue_ReturnTotalValue.java

--- Backpack_Classic0or1_MaxTotalValue_ReturnTotalValue.java
+++ Backpack_Classic0or1_MaxTotalValue_ReturnTotalValue.java
@@ -68,3 +68,21 @@ public class Solution {
         return maxTotalValue;
     }
 }
+
+
+只用一行dp数组，不用矩阵，
+然后从右往左填：
+
+int[] dp = new int[m + 1]
+int n = nums.length;
+
+for (int i = 1; i <= size; i++) {
+    for (int j = m; j >= 0; j--) {
+        if (j >= nums[i - 1]) {
+            dp[j] = Math.max(dp[j], dp[j - nums[i - 1]] + V[i - 1]);
+        }
+    }
+}
+return dp[m]
+
+nums是重量，V数组是values
