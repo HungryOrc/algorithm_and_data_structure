13f5af4a0248f44518f552e0b8c8bc39f1aacd7d
HungryOrc <shu.da.the119@gmail.com>
2018-12-04 23:23:00 -0500

    Update Backpack_Naive0or1_MaxTotalSize_ReturnTotalSize.java

--- Backpack_Naive0or1_MaxTotalSize_ReturnTotalSize.java
+++ Backpack_Naive0or1_MaxTotalSize_ReturnTotalSize.java
@@ -81,3 +81,10 @@ dp[i][j] means by using i items, the maximum size we can get with the j size con
 dp[i][j] = Max(dp[i-1][j], dp[i-1][j-A[i]] + A[i])， the second part has a prerequisit: j >= A[i]
 
 我自己写写看！
+
+
+只需要2行空间，我自己写写看！
+
+
+从右往左？更快？我自己试试看
+John老师说可以这样
