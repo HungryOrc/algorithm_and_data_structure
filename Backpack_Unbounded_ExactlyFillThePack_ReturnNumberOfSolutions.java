a73ac028dc4b6fa380734d5fab832ff1ff08af68
HungryOrc <shu.da.the119@gmail.com>
2018-12-11 11:39:57 -0500

    Update Backpack_Unbounded_ExactlyFillThePack_ReturnNumberOfSolutions.java

--- Backpack_Unbounded_ExactlyFillThePack_ReturnNumberOfSolutions.java
+++ Backpack_Unbounded_ExactlyFillThePack_ReturnNumberOfSolutions.java
@@ -1,26 +1,3 @@
-/* 这一类题叫“Unbounded”的意思是，每个item可以被取用 0次 到 无限次。
-背包有最大的容量（对于size或weight）。求有多少种正好填满背包的方法。 */
-
-/* 思路：二维 DP
-int dp[i][s] 的意思是：使用数组里index为0到i的items中的任意几个（可以一个也不选，也可以从中任选几个），
-正好组成总 size = s 的前提下，一共有多少种可能的方法。
-所以 int dp[][] = new int[number of items][capacity of the backpack + 1]。
-
-Base Cases:
-对于数组里的第一个item，
-if(sizes[0] * i <= capacity)，dp[0][sizes[0] * i] = 1，即1种方法
-其中 i = 1, 2, 3...
-其他的 dp[0][s != sizes[0]] 都 = 0，因为不可能实现这些size，所以是0种方法
-    
-Induction Rule: 这里归纳出来两种递归规则，即两种方法
-
-方法1：          
-for (int j = 0; j <= size / sizes[i]; j++) {
-    dp[i][size] += dp[i - 1][size - j * sizes[i]];
-}
-
-方法2：
-
     // 这里分两种情况，
     // 加号左边 dp[i - 1][sum] 表示，sum里面将没有array[i]的任何参与，
     // 加号右边 dp[i][sum - curValue] 表示，sum里面将存在array[i]的一次或者多次参与，
@@ -32,48 +9,6 @@ for (int j = 0; j <= size / sizes[i]; j++) {
     
     dp[i][sum] = dp[i - 1][sum] + dp[i][sum - curValue];
 }
-
-Return: dp[n - 1][capacity]
-        
-Time: O(n * capacity), 其中n是items的个数
-Space: O(capacity)   */
-
-
-// 方法1：
-public class Solution {
-
-    public int backPackIV(int[] sizes, int capacity) {
-        if (sizes == null || sizes.length == 0 || capacity <= 0) {
-            return 0;        
-        }
-        
-        int n = sizes.length;
-        int[][] dp = new int[n][capacity + 1];
-        
-        // base case 1
-        for (int i = 1; i <= capacity / sizes[0]; i++) {
-            dp[0][sizes[0] * i] = 1;
-        }
-        
-        // base case 2
-        for (int i = 0; i < n; i++) {
-            dp[i][0] = 1;
-        }
-        
-        for (int i = 1; i < n; i++) {
-            for (int size = 1; size <= capacity; size++) {
-                int curSize = sizes[i];
-                
-                for (int j = 0; j <= size / curSize; j++) {
-                    dp[i][size] += dp[i - 1][size - j * curSize];
-                }
-            }
-        }
-        
-        return dp[n - 1][capacity];
-    }
-}
-
 
 // 方法2：
 public class Solution {
