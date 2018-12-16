3619138a8ff84fc8ac0234076002c82ec7471340
HungryOrc <shu.da.the119@gmail.com>
2018-12-15 20:55:44 -0500

    Update Backpack_Unbounded_MaxTotalValue_ReturnTotalValue.java

--- Backpack_Unbounded_MaxTotalValue_ReturnTotalValue.java
+++ Backpack_Unbounded_MaxTotalValue_ReturnTotalValue.java
@@ -1,8 +1,8 @@
-/* 这一类题叫“Unbounded”的意思是，每个item可以被取用 0次 到 无限次。
+这一类题叫“Unbounded”的意思是，每个item可以被取用 0次 到 无限次。
 每个item有不止一个属性，比如既有size又有value。
 背包有最大的容量（对于size或weight）。求怎么填能获得最大的总value。背包不必被填满。 */
 
-/* 思路：二维 DP
+思路：二维 DP
 int dp[i][s] 的意思是：使用数组里index为0到i的items中的任意几个（可以一个也不选，也可以从中任选几个），
 正好组成总 size = s 的前提下，最大可能的总 value 是多少。
 所以 int dp[][] = new int[number of items][capacity of the backpack + 1]。
@@ -29,62 +29,9 @@ Return:
     }
     return maxTotalValue;
         
-Time: O(n * capacity), 其中n是items的个数
-Space: O(n * capacity)。可以优化为 O(capacity)，因为dp矩阵里，其实每一次loop只用一行就够了   */
 
 
-public class Solution {
-    public int backPackIII(int[] sizes, int[] values, int capacity) {
-        if (values == null || values.length == 0 || sizes == null || sizes.length == 0 || capacity <= 0) {
-            return 0;        
-        }
-	    
-	    // 这里还应该考虑两个数组长度不同的情况！！！！！写了代码以后留一句注释在这里！！！
-        
-        
-        int n = sizes.length;
-        int[][] dp = new int[n][capacity + 1];
-        
-        // base case 1
-        for (int i = 1; i <= capacity / sizes[0]; i++) {
-            dp[0][sizes[0] * i] = values[0] * i;
-        }
-        
-        // base case 2 ---- 这个其实可以不写，因为默认都是 0. 写了只是更能解释清楚思路
-        for (int i = 0; i < n; i++) {
-            dp[i][0] = 0;
-        }
-        
-        for (int i = 1; i < n; i++) {
-            for (int size = 1; size <= capacity; size++) {
-                
-                int curSize = sizes[i];
-                int curValue = values[i];
-                
-                if (size - curSize >= 0) {
-                    dp[i][size] = Math.max(dp[i - 1][size], dp[i - 1][size - curSize] + curValue);
-                    dp[i][size] = Math.max(dp[i][size], dp[i][size - curSize] + curValue);
-                } else {
-                    dp[i][size] = dp[i - 1][size];
-                }
-            }
-        }
-        
-        int maxTotalValue = 0;
-        for (int size = 1; size <= capacity; size++) {
-            maxTotalValue = Math.max(maxTotalValue, dp[n - 1][size]);
-        }
-        return maxTotalValue;
-    }
-}
 
-
-改进方法：
-
-dp[i][j] = Max(dp[i-1][j], dp[i][j-A[i]] + V[i])
-    
-一行以内自己填就行，不用k个来loop，一行以内右边的基于左边的来填
-然后对于总的矩阵来说，从上到下，从左到右填
 
 上面那个地推公式的空间可以进一步优化：
 用滚动数组，上一行的信息留存在dp[j]的前一次occurence里面了！！！
