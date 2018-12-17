862e00f9a69027f12d0066cbe0dfdc8589fb25e2
HungryOrc <shu.da.the119@gmail.com>
2018-12-16 22:09:58 -0500

    Update Backpack_Classic0or1_MaxTotalValue_LimitedNumberOfItems_ReturnTotalValue.java

--- Backpack_Classic0or1_MaxTotalValue_LimitedNumberOfItems_ReturnTotalValue.java
+++ Backpack_Classic0or1_MaxTotalValue_LimitedNumberOfItems_ReturnTotalValue.java
@@ -1,36 +1,5 @@
-/* 所谓 “Classic 0/1背包问题”，0/1 是指每个item最多取一次，Classic的意思是每个item有不止一个属性，比如既有size又有value。
-背包有最大的容量（对于size）。然后这一题的特殊之处在于，限制了最多只能选取 m 个items。
-求怎么填能获得最大的总value。背包不必被填满。 */
-
-思路：三维 DP
-int dp[i][j][s] 的意思是：使用 index为0到i 的 items 中的 正好j个items（总共最多能选用 m 个items），
-正好组成总 size = s 的前提下，最大可能的总 value 是多少。
-所以 int dp[][][] = new int[number of items][m + 1][capacity of the backpack + 1]。
-
-Base Cases: 对于只选取了1个item的情况（设一共有n个items）：
-    for (int i = 0; i < n; i++) {
-        if (size[i] <= capacity)
-            dp[i][1][sizes[i]] = index为0到i的items里,size等于sizes[i]的items里,value最大的那个item的value;
-    }
-    
-Induction Rule:
-dp[i][j][sum] = Math.max(dp[i - 1][j][sum], dp[i - 1][j - 1][sum - curSize] + curValue);
-
-Return:
-注意，这里不是返回 dp[n - 1][m][capacity] ！！ 
-因为获得总value最大时，总items的个数未必是 m ！！！ 总size也未必是 capacity ！！！
-    for (all the possible number of items, and all the possible total size) {
-        maxTotalValue = Math.max(maxTotalValue, dp[n - 1][numuber of items][total size]);
-    }
-    return maxTotalValue;
-        
-Time: O(n * m * capacity), 其中n是items的个数，m是最多可以有几个items
-Space: O(n * m * capacity)。可以优化为 O(m * capacity)
-
-
 public class Solution {
-     
-    public int backPack(int capacity, int m, int[] sizes, int[] values) {
+    public int backPack_UnknownQuestionNumber(int capacity, int m, int[] sizes, int[] values) {
         int n = sizes.length;
         
         int[][][] dp = new int[n][m + 1][capacity + 1];
