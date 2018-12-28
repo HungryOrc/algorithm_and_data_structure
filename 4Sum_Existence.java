71c320a2cc5bcba8fa04bf5ccc3cd8bdb42768f0
HungryOrc <shu.da.the119@gmail.com>
2018-12-28 16:43:37 -0500

    Update 4Sum_Existence.java

--- 4Sum_Existence.java
+++ 4Sum_Existence.java
@@ -1,19 +1,8 @@
-/* 条件：
-给的元素可能有重复。可以是正负整数或小数。
-每个元素最多用一次。
 
-Determine if there exists a set of four elements in a given array that sum to the given target number.
-Assumptions: The given array is not null and has length of at least 4
+看lintcode或者laicode上有没有这题
 
-Examples:
-A = {1, 2, 2, 3, 4}, target = 9, return true(1 + 2 + 2 + 4 = 8)
-A = {1, 2, 2, 3, 4}, target = 12, return false  */
+然后在leetcode和lintcode上搜索 4 sum，four sum
 
-
-/* 思路：很巧妙！用HashMap 以及2层for-loop，时间 O(n^2) 就搞定了！
-
-把想象中的4个数分为2个pair，分别为 p1L, p1R, p2L, p2R. 它们的和需要等于特定的target。然而还有一个重要的要求，就是
-p2L 的index 必须 > p1R 的index ！！！   */
 
 // helper class
 class Pair {
