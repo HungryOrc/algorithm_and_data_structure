c7fea5cee127afb863565b887a3c6006917ff1c8
HungryOrc <shu.da.the119@gmail.com>
2019-01-16 01:34:02 -0500

    Update CheckCompleteBinaryTree.java

--- CheckCompleteBinaryTree.java
+++ CheckCompleteBinaryTree.java
@@ -37,4 +37,5 @@ public class Solution {
  if any node has right, but left child is null, then return false
  
  if any node has less than 2 children, then if any node after him in the queue has any child, then return false
+         for example in the above graph, the first tree, after 8, there are 1 and 4 in the queue, it is fine, as long as 1 and 4 dont have any children
 }
