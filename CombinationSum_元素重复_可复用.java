4982a572a16167221f88892e0eba493e15ff474e
HungryOrc <shu.da.the119@gmail.com>
2019-04-09 23:55:32 -0400

    Update CombinationSum_元素重复_可复用.java

--- CombinationSum_元素重复_可复用.java
+++ CombinationSum_元素重复_可复用.java
@@ -1,7 +1,4 @@
-// 数组里的元素可能有重复
-// 在一个组合里，每个元素都可以出现任意次。所以每个元素在数组里出现多少次其实没有意义
-// 在一个组合里，总的元素个数不限
-// 在一个组合里，每个元素的出现顺序如果改变，还算是同一个组合
+
 
 /* Given a set of candidate numbers (C) and a target number (T), 
 find all unique combinations in C where the candidate numbers sums to T.
@@ -18,10 +15,7 @@ Given candidate set [2,3,6,7] and target 7, a solution set is:
 [2, 2, 3] */
 
 public class Solution {
-    
-    // 方法1：数组不去重
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        
         List<List<Integer>> result = new ArrayList<>();
         if (candidates == null || candidates.length == 0) {
             return result;
@@ -32,12 +26,9 @@ public class Solution {
         dfs(candidates, 0, target, new ArrayList<Integer>(), result);
         return result;
     }
-    private void dfs(int[] candidates,
-                int startIndex,
-                int remainTarget,
-                ArrayList<Integer> combination,
-                List<List<Integer>> result) {
-        
+    
+    private void dfs(int[] candidates, int startIndex, int remainTarget,
+        ArrayList<Integer> combination, List<List<Integer>> result) {
         if (remainTarget == 0) {
             result.add(new ArrayList<Integer>(combination)); // 注意！要new一个！
             return;
@@ -48,26 +39,6 @@ public class Solution {
             if (candidates[i] > remainTarget) {
                 return;
             }
-            
-            // 精华所在！！！下面这个if语句是为了保证进入最终result的每个组合都是互不相同的！！！
-            // 只有当数组里有重复元素时，才有这个if语句存在的必要！
-            // 而这个if语句的存在，与数组里的每个元素能被使用一次还是无限次，无关！
-            // 每个元素能被使用一次还是无限次，是靠下文的 startIndex 在下一轮dfs里
-            // +1 还是 不+1 来区分实现的
-            
-            // 如果数组是 1, 2, 2, 4, 7, 9, 9, 9, 12
-            //
-            // 如果当前的 start index 在 1 那里，则在本次的 for 循环里，
-            // 第一个2会入选，第二个2不会入选；第一个9会入选，第二三个9不会入选
-            //
-            // 如果当前的 start index 在 第一个2 那里，则在本次的 for 循环里，
-            // 第一个2会入选，第二个2不会入选；第一个9会入选，第二三个9不会入选
-            //
-            // 如果当前的 start index 在第二个2 那里，则在本次的 for 循环里，
-            // 第二个2会入选；第一个9会入选，第二三个9不会入选
-            if (i > 0 && candidates[i] == candidates[i - 1]) {
-                continue;
-            }
         
             combination.add(candidates[i]);
             // 重要！一个元素要无限次复用，
@@ -76,60 +47,4 @@ public class Solution {
             combination.remove(combination.size() - 1);
         }
     }
-    
-    
-    // 方法2：先把数组去重
-    // Ref: http://www.jiuzhang.com/solutions/combination-sum/
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> results = new ArrayList<>();
-        if (candidates == null || candidates.length == 0) {
-            return results;
-        }
-        
-        int[] nums = removeDuplicates(candidates);
-        
-        dfs(nums, 0, new ArrayList<Integer>(), target, results);
-        
-        return results;
-    }
-    // 注意这个去重函数的写法！空间上是 in place 的！
-    private int[] removeDuplicates(int[] candidates) {
-        Arrays.sort(candidates);
-        
-        int index = 0;
-        for (int i = 0; i < candidates.length; i++) {
-            // 关键在这个if里面！！
-            if (candidates[i] != candidates[index]) {
-                index ++;
-                candidates[index] = candidates[i];
-            }
-        }
-        
-        int[] nums = new int[index + 1];
-        for (int i = 0; i < index + 1; i++) {
-            nums[i] = candidates[i];
-        }
-        
-        return nums;
-    }
-    private void dfs(int[] nums,
-                     int startIndex,
-                     List<Integer> combination,
-                     int remainTarget,
-                     List<List<Integer>> results) {
-        if (remainTarget == 0) {
-            results.add(new ArrayList<Integer>(combination));
-            return;
-        }
-        
-        for (int i = startIndex; i < nums.length; i++) {
-            if (remainTarget < nums[i]) {
-                return;
-            }
-            combination.add(nums[i]);
-            dfs(nums, i, combination, remainTarget - nums[i], results);
-            combination.remove(combination.size() - 1);
-        }
-    }
-    
 }
